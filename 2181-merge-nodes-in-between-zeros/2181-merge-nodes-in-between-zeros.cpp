d65b03ee6ce422988d15609db7ff9df3421b7a7c
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-04-08 12:11:55 +0530

    Time: 703 ms (90.35%), Space: 253.2 MB (76.26%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -1,52 +1,28 @@
-/**
- *Definition for singly-linked list.
- *struct ListNode {
- *  int val;
- *  ListNode * next;
- *  ListNode() : val(0), next(nullptr) {}
- *  ListNode(int x) : val(x), next(nullptr) {}
- *  ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
- */
-class Solution
-{
-    public:
-        ListNode* mergeNodes(ListNode *head)
-        {
-            if (!head)
-            {
-                return head;
-            }
-            ListNode *newHead = NULL;
-            ListNode *tail = NULL;
-            int sum = 0;
-            while (head != NULL and head->val == 0)
-            {
-                head = head->next;
-            }
-            while (head)
-            {
-                if (head->val != 0)
-                {
-                    sum = sum + head->val;
-                }
-                else
-                {
-                    ListNode *temp = new ListNode(sum);
-                    if (newHead == NULL)
-                    {
-                        newHead = temp;
-                        tail = newHead;
-                    }
-                    else
-                    {
-                        tail->next = temp;
-                        tail = temp;
-                    }
-                    sum = 0;
-                }
-                head = head->next;
-            }
-            return newHead;
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        if(!head){
+            return head;
         }
+        while(head->val == 0){
+            head = head->next;
+        }
+        int sum = 0;
+        ListNode* l = head;
+        ListNode* r = head;
+        while(r){
+            if(r->val != 0){
+                if(l != r){
+                    l->val = l->val + r->val;
+                }else{
+                    l->val = r->val;
+                }
+            }else{
+                l->next = r->next;
+                l = l->next;
+            }
+            r = r->next;
+        }
+        return head;
+    }
 };
