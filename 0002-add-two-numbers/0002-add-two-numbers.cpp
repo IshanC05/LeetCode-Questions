72d1845d967fcfb7f703c862420e28c9b8840ec2
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-04-10 18:08:44 +0530

    Time: 28 ms (90.82%), Space: 71.5 MB (14.05%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -14,65 +14,28 @@ public:
         int carry = 0;
         ListNode* ans = NULL;
         ListNode* tail = NULL;
-        while(l1 and l2){
-            int sum = carry + l1->val + l2->val;
-            carry = 0;
-            if(sum > 9){
-                carry = carry + 1;
-                sum = sum % 10;
+        while(l1 || l2 || carry){
+            int sum = 0;
+            if(l1){
+                sum = sum + l1->val;
+                l1 = l1->next;
             }
+            if(l2){
+                sum = sum + l2->val;
+                l2 = l2->next;
+            }
+            sum = sum + carry;
+            carry = sum / 10;
+            sum = sum % 10;
             ListNode* temp = new ListNode(sum);
-            if(!ans){
+            if(!tail){
                 ans = temp;
-                tail = ans;
+                tail = temp;
             }else{
                 tail->next = temp;
                 tail = temp;
             }
-            l1 = l1->next;
-            l2 = l2->next;
         }
-        if(l1){
-            while(l1 and carry){
-                l1->val = carry + l1->val;
-                carry = 0;
-                if(l1->val > 9){
-                    carry = carry + 1;
-                    l1->val = l1->val % 10;
-                }
-                tail->next = l1;
-                tail = l1;
-                l1 = l1->next;
-            }
-            while(l1){
-                tail->next = l1;
-                tail = l1;
-                l1 = l1->next;
-            }
-        }
-        if(l2){
-            while(l2 and carry){
-                l2->val = carry + l2->val;
-                carry = 0;
-                if(l2->val > 9){
-                    carry = carry + 1;
-                    l2->val = l2->val % 10;
-                }
-                tail->next = l2;
-                tail = l2;
-                l2 = l2->next;
-            }
-            while(l2){
-                tail->next = l2;
-                tail = l2;
-                l2 = l2->next;
-            }
-        }
-        if(carry){
-            tail->next = new ListNode(carry);
-            tail = tail->next;
-        }
-        tail->next = NULL;
         return ans;
     }
 };
