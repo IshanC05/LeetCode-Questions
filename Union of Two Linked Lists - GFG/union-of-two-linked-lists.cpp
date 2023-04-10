0f4fbb208d4797eefbb75e04ce02ef71b7e70573
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-04-10 20:27:29 +0530

    Added solution - LeetHub

---
+++ Union of Two Linked Lists - GFG/union-of-two-linked-lists.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+struct Node* makeUnion(struct Node* head1, struct Node* head2);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* first = buildList(n);
+        
+        cin>>m;
+        Node* second = buildList(m);
+        
+        printList(makeUnion(first,second));
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+struct Node* makeUnion(struct Node* head1, struct Node* head2)
+{
+    // code here
+    set<int>elements;
+    Node* l = head1;
+    while(l){
+        elements.insert(l->data);
+        l = l->next;
+    }
+    
+    l = head2;
+    while(l){
+        elements.insert(l->data);
+        l = l->next;
+    }
+    Node* ans = NULL;
+    Node* tail = NULL;
+    for(auto x : elements){
+        Node* temp = new Node(x);
+        if(!ans){
+            ans = temp;
+            tail = ans;
+        }else{
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+    return ans;
+}
