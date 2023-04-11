d0bbba69a3a97568eb426813f3feb4f1d9e8e49d
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-04-11 12:30:38 +0530

    Time: 30 ms (24.99%), Space: 8.6 MB (11.18%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,33 +1,27 @@
 /**
- *Definition for singly-linked list.
- *struct ListNode {
- *   int val;
- *   ListNode * next;
- *   ListNode() : val(0), next(nullptr) {}
- *   ListNode(int x) : val(x), next(nullptr) {}
- *   ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
-class Solution
-{
-    public:
-        ListNode* reverseList(ListNode *head)
-        {
-            if (!head || head->next == NULL)
-            {
-                return head;
-            }
-            ListNode *prev = NULL;
-            ListNode *curNode = head;
-            ListNode *nextNode = head->next;
-            while (curNode->next)
-            {
-                curNode->next = prev;
-                prev = curNode;
-                curNode = nextNode;
-                nextNode = nextNode->next;
-            }
-            curNode->next = prev;
-            return curNode;
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next){
+            return head;
         }
+        ListNode* cur = head;
+        ListNode* smallAns = reverseList(head->next);
+        ListNode* temp = smallAns;
+        while(temp->next){
+            temp = temp->next;
+        }
+        temp->next = cur;
+        cur->next = NULL;
+        return smallAns;
+    }
 };
