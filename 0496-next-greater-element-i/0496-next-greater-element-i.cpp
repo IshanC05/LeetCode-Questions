5114da681b3e41cd447ecccf138f569029f38dfa
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-04-14 11:49:33 +0530

    Time: 10 ms (46.11%), Space: 8.8 MB (65.55%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,32 +3,19 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int>m;
         stack<int>st;
-        for(int i=nums2.size()-1; i>=0; i--){
-            if(st.empty()){
-                st.push(nums2[i]);
-                m[nums2[i]] = -1;
-            }else{
-                if(st.top() > nums2[i]){
-                    m[nums2[i]] = st.top();
-                    st.push(nums2[i]);
-                }else{
-                    while(!st.empty()){
-                        if(st.top() > nums2[i]){
-                            m[nums2[i]] = st.top();
-                            st.push(nums2[i]);
-                            break;
-                        }
-                        st.pop();
-                    }
-                    if(st.empty()){
-                        m[nums2[i]] = -1;
-                        st.push(nums2[i]);
-                    }
-                }
+        for(int i=0;i<nums2.size();i++){
+            while(!st.empty() and st.top() < nums2[i]){
+                m[st.top()] = nums2[i];
+                st.pop();
             }
+            st.push(nums2[i]);
         }
         for(int i=0;i<nums1.size();i++){
-            nums1[i] = m[nums1[i]];
+            if(m.count(nums1[i])){
+                nums1[i] = m[nums1[i]];
+            }else{
+                nums1[i] = -1;
+            }
         }
         return nums1;
     }
