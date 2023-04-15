d55d4cab07959b4906855be58cc6c15e8fd535a7
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-04-15 21:04:04 +0530

    Time: 37 ms (75.27%), Space: 36.1 MB (73.34%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -6,13 +6,12 @@ class Solution
             int n = nums.size(), ans = 0, cur = 0, i = 0;
             while (i < n)
             {
-                while (i < n and nums[i] == 1)
-                {
+                if(nums[i] == 1){
                     ++cur;
-                    ++i;
+                    ans = max(ans, cur);
+                }else{
+                    cur = 0;
                 }
-                ans = max(ans, cur);
-                cur = 0;
                 ++i;
             }
             ans = max(ans, cur);
