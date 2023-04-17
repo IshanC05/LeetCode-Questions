26af1ad9017753a2710d49a10e7c0f940c37a937
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-04-17 14:02:05 +0530

    Time: 188 ms (85.76%), Space: 10.2 MB (71.01%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -7,7 +7,7 @@ public:
         vector<bool>isPrime(n+1, true);
         for(int i=2;i*i<=n;i++){
             if(isPrime[i]){
-                for(int j=2*i;j<=n;j=j+i){
+                for(int j=i*i;j<=n;j=j+i){
                     isPrime[j] = false;
                 }
             }
