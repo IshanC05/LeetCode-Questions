81bf17319196f6d5953a6bdf402596213ae32834
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-05-04 12:42:33 +0530

    Time: 236 ms (42.43%), Space: 68.2 MB (5.00%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,27 +1,28 @@
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        int n = nums.size();
-        // if(n == 0){
-        //     return n;
-        // }
-        unordered_map<int,bool>present,checked;
-        for(int i = 0; i < n; i++){
-            present[nums[i]] = true;
-        }
-        int longestConsecutiveSequence = 0;
-        for(int i = 0; i < n; i++){
-            int x = nums[i];
-            if(present.find(x - 1) == present.end() and checked.find(x) == checked.end()){
-                int curr = 1;
-                while(present.find(x + 1) != present.end()){
-                    checked[x] = true;
-                    ++curr;
-                    ++x;
-                }
-                longestConsecutiveSequence = max(longestConsecutiveSequence, curr);
+class Solution
+{
+    public:
+        int longestConsecutive(vector<int> &nums)
+        {
+            unordered_map<int, bool> present, checked;
+            for (int i: nums)
+            {
+                present[i] = true;
             }
+            int maxLength = 0;
+            for (int x: nums)
+            {
+                if (!present[x - 1] and!checked[x])
+                {
+                    int curr = 0;
+                    while (present[x])
+                    {
+                        ++curr;
+                        checked[x] = true;
+                        ++x;
+                    }
+                    maxLength = max(maxLength, curr);
+                }
+            }
+            return maxLength;
         }
-        return longestConsecutiveSequence;
-    }
 };
