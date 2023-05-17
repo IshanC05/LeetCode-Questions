c59c822bc460dfcaa6dd87b52089e7bdad30796a
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-05-17 16:39:48 +0530

    Time: 445 ms (22.98%), Space: 142.3 MB (94.70%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,26 +1,33 @@
 class NumMatrix
 {
+    vector<vector < int>> matrix;
     public:
-    vector<vector < int>> prefixSum;
-    NumMatrix(vector<vector < int>> &matrix)
-    {
-        int m = matrix.size(), n = matrix[0].size();
-        prefixSum.resize(m + 1);
-        for (int i = 0; i <= m; i++)
+        NumMatrix(vector<vector < int>> &matrix)
         {
-            prefixSum[i].resize(n + 1, 0);
-            for (int j = 0; j <= n; j++)
+            for (int i = 0; i < matrix.size(); i++)
             {
-                if (i == 0 || j == 0) continue;
-                prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + matrix[i - 1][j - 1];
+                for (int j = 1; j < matrix[0].size(); j++)
+                {
+                    matrix[i][j] += matrix[i][j - 1];
+                }
             }
+
+            for (int i = 1; i < matrix.size(); i++)
+            {
+                for (int j = 0; j < matrix[0].size(); j++)
+                {
+                    matrix[i][j] += matrix[i - 1][j];
+                }
+            }
+
+            this->matrix = matrix;
         }
-    }
 
     int sumRegion(int row1, int col1, int row2, int col2)
     {
-        row1++, row2++, col1++, col2++;
-        return prefixSum[row2][col2] - prefixSum[row2][col1 - 1] - prefixSum[row1 - 1][col2] + prefixSum[row1 - 1][col1 - 1];
+        int total = matrix[row2][col2];
+        int extra = (col1 != 0 ? matrix[row2][col1 - 1] : 0) + (row1 != 0 ? matrix[row1 - 1][col2] : 0) - ((row1 != 0 && col1 != 0) ? matrix[row1 - 1][col1 - 1] : 0);
+        return total - extra;
     }
 };
 
