fb7ba254842d6d4594f1a0d05aa6303c1d54a8cf
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-05-23 13:32:15 +0530

    Time: 14 ms (42.69%), Space: 9.7 MB (48.81%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
-        vector<int>freq(26, 0);
-        for(int i = 0; i < words[0].size(); i++){
-            ++freq[words[0][i] - 'a'];
+        vector<int>mainCounter(26, 0);
+        string start = words[0];
+        for(char i : start){
+            ++mainCounter[i - 'a'];
         }
         for(int i = 1; i < words.size(); i++){
-            string temp = words[i];
-            vector<int>tempFreq(26, 0);
-            for(int j = 0; j < temp.size(); j++){
-                ++tempFreq[temp[j] - 'a'];
+            vector<int>temp(26, 0);
+            string word = words[i];
+            
+            for(char j : word){
+                ++temp[j - 'a'];
             }
+            
             for(int k = 0; k < 26; k++){
-                freq[k] = min(freq[k], tempFreq[k]);
+                mainCounter[k] = min(mainCounter[k], temp[k]);
             }
         }
+        
         vector<string>ans;
         for(int i = 0; i < 26; i++){
-            while(freq[i]--){
+            while(mainCounter[i]--){
                 string t = "";
-                t.push_back((char)(i + 'a'));
+                char d = i + 'a';
+                t.push_back(d);
                 ans.push_back(t);
             }
         }
