56f615eef494c4fc16b149e26ef297530a384731
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-06-12 12:06:35 +0530

    Time: 510 ms (5.04%), Space: 142.9 MB (5.04%) - LeetHub

---
+++ 2611-mice-and-cheese/2611-mice-and-cheese.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
+        int n = reward1.size();
+        vector<vector<int>>temp;
+        int ans = 0;
+        for(int i : reward2){
+            ans += i;
+        }
+        
+        for(int i = 0; i < n; i++){
+            temp.push_back({reward1[i] - reward2[i], i});
+        }
+        
+        sort(temp.rbegin(), temp.rend());
+        
+        for(int j = 0; j < n; j++){
+            if(k == 0){
+                break;
+            }
+            int idx = temp[j][1];
+            ans -= reward2[idx];
+            ans += reward1[idx];
+            --k;
+        }
+        
+        return ans;
+    }
+};
