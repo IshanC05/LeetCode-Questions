d6ab3fdd924ca8e8d1616201273e2be2ae46d69f
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-06-20 12:05:24 +0530

    Time: 271 ms (31.54%), Space: 137 MB (40.41%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-
-        int windowLen = 2*k + 1, n = nums.size();
+        int windowLen = 2 * k + 1, n = nums.size();
         vector<int>ans(n, -1);
-
-        long long i = 0, j = 0, mid = 0, sum = 0;
-        while(j < n){
-            sum += nums[j];
-            long long curLen = j - i + 1;
-            if(curLen < windowLen){
-                ++j;
-            }else if(curLen == windowLen){
-                mid = i + (j - i) / 2;
-                ans[mid] = sum / windowLen;
-                sum -= nums[i];
-                ++i;
-                ++j;
+        
+        if(k == 0) return nums;
+        if(windowLen > n) return ans;
+        
+        vector<long long>prefix(n, 0);
+        prefix[0] = nums[0];
+        
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+        
+        for(int i = 0; i < n; i++){
+            int l = i - k, r = i + k;
+            if(l < 0 || r >= n){
+                continue;
+            }else{
+                if(l == 0){
+                    ans[i] = prefix[r] / windowLen;
+                }else{
+                    ans[i] = (prefix[r] - prefix[l - 1]) / windowLen;
+                }
             }
         }
+        
         return ans;
     }
 };
