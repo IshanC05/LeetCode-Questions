b952128aac36933c0805a0da8aff88354d33d37d
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-06-23 18:13:18 +0530

    Time: 57 ms (14.99%), Space: 89.7 MB (41.77%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    vector<int> merge2Arrays(vector<int>n1, vector<int>n2){
-        vector<int>ans(n1.size() + n2.size());
-        int m=n1.size(), n = n2.size();
-        int i=0,j=0,k=0;
-        while(i < m and j < n){
-            if(n1[i] > n2[j]){
-                ans[k] = n2[j];
-                ++j;
-            }else{
-                ans[k] = n1[i];
-                ++i;
-            }
-            ++k;
-        }
-        while(i < m){
-            ans[k++] = n1[i++];
-        }
-        while(j < n){
-            ans[k++] = n2[j++];
-        }
-        return ans;
-    }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>mergedArr = merge2Arrays(nums1, nums2);
-        int d = mergedArr.size();
-        if(d & 1){
-            return mergedArr[d / 2];
+        
+        int m = nums1.size(), n = nums2.size();
+        vector<int>ans(m + n, 0);
+        int k = 0;
+        int i = 0, j = 0;
+        
+        while(i < m and j < n){
+            if(nums1[i] < nums2[j]){
+                ans[k++] = nums1[i++];
+            }else{
+                ans[k++] = nums2[j++];
+            }
         }
-        return ((mergedArr[d / 2] + mergedArr[(d / 2) - 1]) / 2.0);
+        
+        while(i < m){
+            ans[k++] = nums1[i++];
+        }
+        
+        while(j < n){
+            ans[k++] = nums2[j++];
+        }
+        int mid = (m + n) / 2;
+        if((m + n) % 2 == 0){
+            return (ans[mid] + ans[mid - 1]) / 2.0;
+        }
+        return ans[(m + n) / 2];
     }
 };
