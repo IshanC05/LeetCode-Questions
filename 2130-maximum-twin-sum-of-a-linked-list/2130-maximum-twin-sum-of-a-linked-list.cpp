b0096566180531d4e467361de68fa356d8425044
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-06-29 15:11:01 +0530

    Time: 267 ms (83.95%), Space: 116.5 MB (98.75%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,27 +10,46 @@
  */
 class Solution {
 public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next)
+            return head;
+        
+        ListNode* prev = nullptr, *curr = head, *nextNode = head->next;
+        
+        while(nextNode){
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+            nextNode = nextNode->next;
+        }
+        
+        curr->next = prev;
+        
+        return curr;
+    }
     int pairSum(ListNode* head) {
-        if(!head){
-            return 0;
+        
+        ListNode* slow = head, *fast = head, *prev = nullptr;
+        
+        int ans = 0;
+        
+        while(fast && fast->next){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        ListNode* temp = head, *length = head;
-        int res = 0, i = 0, n = 0;
-        while(length){
-            ++n;
-            length = length->next;
+        
+        prev->next = nullptr;
+        
+        slow = reverseList(slow);
+        prev = head;
+        
+        while(prev){
+            ans = max(ans, prev->val + slow->val);
+            prev = prev->next;
+            slow = slow->next;
         }
-        stack<int>st;
-        while(i < n / 2){
-            st.push(temp->val);
-            ++i;
-            temp = temp->next;
-        }
-        while(temp and !st.empty()){
-            res = max(res, st.top() + temp->val);
-            st.pop();
-            temp = temp -> next;
-        }
-        return res;
+        
+        return ans;
     }
 };
