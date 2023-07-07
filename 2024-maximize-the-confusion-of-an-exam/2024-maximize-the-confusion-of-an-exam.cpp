24557eecde88cc3380c658a86bea13d8fbbf4fcf
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-07-07 10:55:36 +0530

    Time: 28 ms (64.43%), Space: 10.2 MB (33.14%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,39 +1,44 @@
-class Solution {
-public:
-    
-    int helper(string &s, int k, char a, char b){
-        
-        int i = 0, j = 0, n = s.size();
-        
-        int ans = 0, count = 0;
-        
-        while(j < n){
-            
-            if(s[j] == b){
-                ++count;
-            }
-            
-            while(count > k){
-                if(s[i] == b)   --count;
+class Solution
+{
+    public:
+
+        int helper(string &s, int k, char a, char b)
+        {
+
+            int i = 0, j = 0, n = s.size();
+
+            int ans = 0, count = 0;
+
+            while (j < n)
+            {
+
+                if (s[j] == b)
+                {
+                    ++count;
+                }
+
+                while (count > k)
+                {
+                    if (s[i] == b) --count;
+
+                    ++i;
+                }
+
+                ans = max(ans, j - i + 1);
                 
-                ++i;
+                ++j;
             }
-            
-            ans = max(ans, j - i + 1);
-            ++j;
+
+            return ans;
         }
-        
-        return ans;        
-    }
-    
-    
-    int maxConsecutiveAnswers(string answerKey, int k) {
-        
+
+    int maxConsecutiveAnswers(string answerKey, int k)
+    {
+
         int smallAns1 = helper(answerKey, k, 'T', 'F');
-        
+
         int smallAns2 = helper(answerKey, k, 'F', 'T');
-        
+
         return max(smallAns1, smallAns2);
-        
     }
 };
