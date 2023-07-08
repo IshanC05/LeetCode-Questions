0f9910f21acd6c020dabdb40f32540bd3e67c778
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-07-08 11:39:00 +0530

    Time: 72 ms (84.54%), Space: 20.5 MB (83.48%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -12,7 +12,7 @@ public:
         
         for(int i = 0; i < 26; i++){
             
-            if((freq1[i] != 0 && freq2[i] == 0) || (freq1[i] == 0 && freq2[i] != 0))    return false;
+            if((freq1[i] && !freq2[i]) || (!freq1[i] && freq2[i]))    return false;
             
         }
         
