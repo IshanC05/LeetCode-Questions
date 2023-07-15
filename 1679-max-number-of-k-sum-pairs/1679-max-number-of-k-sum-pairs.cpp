cac3d4b2cadb7809142f613e314c7d5ee08452a4
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-07-15 12:42:37 +0530

    Time: 202 ms (27.79%), Space: 67.9 MB (29.51%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        int n = nums.size(), res = 0;
+        
         unordered_map<int,int>mp;
-        for(int i = 0; i < n; i++){
+        
+        int ans = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            
             int target = k - nums[i];
+            
             if(mp.find(target) != mp.end()){
-                ++res;
+                
+                ++ans;
+                
                 --mp[target];
-                if(mp[target] <= 0){
-                    mp.erase(target);
-                }
-            }else{
-                ++mp[nums[i]];
-            }
+                
+                if(mp[target] == 0) mp.erase(target);
+                
+            }else   ++mp[nums[i]];
+            
         }
-        return res;
+        
+        return ans;
+        
     }
 };
