24d170bd88f9220a9f1efc272071c0aa3c867213
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-07-19 13:20:56 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (19.70%) - LeetHub

--- 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.cpp
+++ 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.cpp
@@ -1,10 +1,56 @@
 class Solution {
 public:
-    string strWithout3a3b(int A, int B) {
-        if(A == 0) return string(B, 'b');
-        else if(B == 0) return string(A, 'a');
-        else if(A == B) return "ab" + strWithout3a3b(A - 1, B - 1);
-        else if(A > B)  return "aab" + strWithout3a3b(A - 2, B - 1);
-        else return "bba" + strWithout3a3b(A - 1, B - 2);
+    string strWithout3a3b(int a, int b) {
+        
+        string ans = "";
+        
+        while(a && b){
+            
+            if(a > b){
+                
+                ans += "aab";
+                
+                a -= 2;
+                
+                --b;
+                
+            }else if(b > a){
+                
+                ans += "bba";
+                
+                b -= 2;
+                
+                --a;
+                
+            }else{
+                
+                ans += "ab";
+                
+                --a;
+                
+                --b;
+                
+            }
+            
+        }
+        
+        while(a){
+            
+            ans += "a";
+            
+            --a;
+            
+        }
+        
+        while(b){
+            
+            ans += "b";
+            
+            --b;
+            
+        }
+        
+        return ans;
+        
     }
 };
