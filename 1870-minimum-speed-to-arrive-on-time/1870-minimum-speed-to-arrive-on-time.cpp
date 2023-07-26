a94b2322f448962810f3ebdc86b488ef69cf477d
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-07-26 10:45:44 +0530

    Time: 328 ms (64.46%), Space: 101.5 MB (26.33%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool helper(vector<int>&arr, int speed, double hour){
+        
+        double currHours = 0.0;
+        
+        for(int i = 0; i < arr.size(); i++){
+            
+            double timeTaken = ((double) arr[i]) / ((double) speed);
+            
+            currHours += (i == arr.size() - 1) ? timeTaken : ceil(timeTaken);
+            
+        }
+        
+        return currHours <= hour;
+        
+    }
+    
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        
+        int l = 1, h = 1e7, ans = -1;
+        
+        while(l <= h){
+            
+            int mid = l + (h - l) / 2;
+            
+            if(helper(dist, mid, hour)){
+                
+                ans = mid;
+                
+                h = mid - 1;
+                
+            }
+            
+            else    l = mid + 1;
+            
+        }
+        
+        return ans;
+        
+    }
+
+};
