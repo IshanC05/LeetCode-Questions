b816eb9d828ca40ea379664be5a25a05fe73e6a0
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-07-27 15:56:24 +0530

    Time: 1077 ms (5.01%), Space: 130.1 MB (11.01%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -1,13 +1,71 @@
 class Solution {
 public:
-    int maxJump(vector<int>& A) {
+    
+    bool helper(vector<int>& arr, int target){
         
-        int res = A[1] - A[0], n = A.size();
+        unordered_set<int>visited;
         
-        for (int i = 2; i < n; ++i)
+        int i = 0, j = 1, n = arr.size();
+        
+        while(j < n){
             
-            res = max(res, A[i] - A[i - 2]);
+            if(arr[j] - arr[i] <= target){}
+            
+            else{
+                
+                visited.insert(arr[j - 1]);
+                
+                i = j - 1;
+                
+            }
+            
+            ++j;
+            
+        }
         
-        return res;
+        if(arr[n - 1] - arr[i] > target)    return false;
+        
+        i = n - 1, j = n - 2;
+        
+        while(j >= 0){
+            
+            if(arr[i] - arr[j] <= target && visited.count(arr[j]) == false){
+                
+                i = j;
+                
+            }
+            
+            --j;
+            
+        }
+        
+        if(arr[i] - arr[0] > target)    return false;
+        
+        return true;
+        
+    }
+    
+    int maxJump(vector<int>& stones) {
+        
+        int n = stones.size(), l = 1, h = stones[n - 1], ans = h;
+        
+        while(l <= h){
+            
+            int mid = l + (h - l) / 2;
+            
+            if(helper(stones, mid)){
+                
+                ans = mid;
+                
+                h = mid - 1;
+                
+            }
+            
+            else    l = mid + 1;
+            
+        }
+        
+        return ans;
+        
     }
 };
