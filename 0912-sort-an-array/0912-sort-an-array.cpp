055b948a86f77a5529180c1b3667cb3ddafc942b
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-08-05 09:54:11 +0530

    Time: 152 ms (89.28%), Space: 72 MB (52.72%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-    void merge(vector<int>& arr, int s1, int e1, int s2, int e2){
-        vector<int>temp;
-        int i = s1, j = s2;
-        while(i <= e1 and j <= e2){
-            if(arr[i] > arr[j]){
-                temp.push_back(arr[j++]);
-            }else{
-                temp.push_back(arr[i++]);
-            }
-        }
-        while(i <= e1){
-            temp.push_back(arr[i++]);
-        }
-        while(j <= e2){
-            temp.push_back(arr[j++]);
-        }
-        int n = temp.size(), k = s1;
-        for(int x : temp){
-            arr[k++] = x;
-        }
-        return;
-    }
-    void mergeSort(vector<int>& arr, int l, int r){
-        if(l >= r){
-            return;
-        }
-        int mid = l + (r - l) / 2;
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid + 1, r);
-        merge(arr, l, mid, mid + 1, r);
-    }
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);
+        priority_queue<int, vector<int>, greater<int>>pq;
+        
+        for(int i : nums)   pq.push(i);
+        
+        int i = 0;
+        
+        while(!pq.empty()){
+            
+            nums[i++] = pq.top();
+            
+            pq.pop();
+            
+        }
+        
         return nums;
     }
 };
