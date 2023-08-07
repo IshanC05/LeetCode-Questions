4cccdfdf4adc23e655fb9e4de11d2d8c0d19543a
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-08-07 10:04:16 +0530

    Time: 126 ms (91.03%), Space: 96.4 MB (36.08%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -8,14 +8,10 @@ public:
             
             if(nums[i] > nums[i + 1])   ans1 = false;
             
-        }
-        
-        for(int i = 0; i < nums.size() - 1; i++){
-            
             if(nums[i] < nums[i + 1])   ans2 = false;
             
         }
-        
+              
         return (ans1 || ans2);
         
     }
