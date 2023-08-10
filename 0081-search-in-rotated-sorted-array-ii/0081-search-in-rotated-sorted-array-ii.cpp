ad500a754b4749498b6bc64db683efc994db4beb
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-08-10 09:41:32 +0530

    Time: 4 ms (69.77%), Space: 14 MB (18.44%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,39 +1,38 @@
-class Solution
-{
-    public:
-        bool search(vector<int> &nums, int target)
-        {
-            int lo = 0;
-            int hi = nums.size() - 1;
-            while (lo <= hi)
-            {
-               	// to avoid duplicates
-                while (lo < hi && nums[lo] == nums[lo + 1])
-                    ++lo;
-                while (lo < hi && nums[hi] == nums[hi - 1])
-                    --hi;
-
-               	// the code below is exactly the same with Problem 33.
-                int mid = lo + (hi - lo) / 2;
-                if (nums[mid] == target)
-                    return true;
-
-                if (nums[mid] >= nums[lo])
-                {
-                    if (target >= nums[lo] && target < nums[mid])
-                        hi = mid - 1;
-                    else
-                        lo = mid + 1;
-                }
-                else
-                {
-                    if (target <= nums[hi] && target > nums[mid])
-                        lo = mid + 1;
-                    else
-                        hi = mid - 1;
-                }
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        
+        int l = 0, h = nums.size() - 1;
+        
+        while(l <= h){
+            
+            while(l < h && nums[l] == nums[l + 1])  ++l;
+            
+            while(h > l && nums[h] == nums[h - 1])  --h;
+            
+            int mid = l + (h - l) / 2;
+            
+            if(nums[mid] == target)     return true;
+            
+            else if(nums[mid] >= nums[l]){
+                
+                if(target >= nums[l] && target < nums[mid]) h = mid - 1;
+                
+                else    l = mid + 1;
+                
             }
-
-            return false;
+            
+            else{
+                
+                if(target <= nums[h] && target > nums[mid]) l = mid + 1;
+                
+                else    h = mid - 1;
+                
+            }
+            
         }
+        
+        return false;
+        
+    }
 };
