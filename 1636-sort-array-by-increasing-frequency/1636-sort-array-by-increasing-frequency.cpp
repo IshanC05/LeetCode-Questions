4a8f1afcbb70b241cc9759c38aa6670dbeed91bf
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-08-10 10:06:56 +0530

    Time: 7 ms (71.29%), Space: 11 MB (91.00%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,52 +1,29 @@
-class Solution {
+unordered_map<int,int>mp;
+
+class Solution {    
 public:
     
-    bool static compare(pair<int,int>a, pair<int,int>b){
+    bool static compare(int a, int b){
         
-        if(a.first == b.first){
+        if(mp[a] == mp[b]){
             
-            if(a.second > b.second) return true;
+            if(a > b) return true;
             
             return false;
             
-        }else{
-            
-            if(a.first < b.first)   return true;
         }
         
-        return false;
+        return mp[a] < mp[b];
                    
     }
     
     vector<int> frequencySort(vector<int>& nums) {
         
-        unordered_map<int,int>mp;
+        mp.clear();
         
         for(int i : nums)   ++mp[i];
-        
-        vector<pair<int,int>>temp;
-        
-        for(auto it : mp){
-            
-            temp.push_back({it.second, it.first});
-            
-        }
-        
-        sort(temp.begin(), temp.end(), compare);
-        
-        int k = 0;
-        
-        for(int i = 0; i < temp.size(); i++){
-            
-            while(temp[i].first){
-                
-                nums[k++] = temp[i].second;
-                
-                --temp[i].first;
-                
-            }
-            
-        }
+
+        sort(nums.begin(), nums.end(), compare);
         
         return nums;
         
