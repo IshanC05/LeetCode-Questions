cdd61d2e93b8905c4c445b6e3a0b5e16e4d68f29
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-08-11 10:44:37 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.cpp
+++ Kth smallest element - GFG/kth-smallest-element.cpp
@@ -13,22 +13,47 @@ class Solution{
     // l : starting index of the array i.e 0
     // r : ending index of the array i.e size-1
     // k : find kth smallest element and return using this function
+    
+    int partition(int arr[], int l, int r, int pivot){
+        
+        int i = l, j = l;
+        
+        while(i <= r){
+            
+            if(arr[i] <= pivot){
+                
+                swap(arr[i], arr[j++]);
+                
+            }
+            
+            ++i;
+            
+        }
+        
+        return j - 1;
+        
+    }
+    
+    int quickSelect(int arr[], int l, int r, int k){
+        
+        if(l > r)   return -1;
+        
+        int pivot = arr[r];
+        
+        int pivotIdx = partition(arr, l, r, pivot);
+        
+        if(pivotIdx == k)   return arr[pivotIdx];
+        
+        else if(pivotIdx > k)   return quickSelect(arr, l, pivotIdx - 1, k);
+        
+        return quickSelect(arr, pivotIdx + 1, r, k);
+        
+    }
+    
     int kthSmallest(int arr[], int l, int r, int k) {
         //code here
-        priority_queue<int,vector<int>,greater<int>>pq;
+        return quickSelect(arr, l, r, k - 1);
         
-        for(int i = l; i <= r; i++){
-            pq.push(arr[i]);
-        }
-        
-        int ans;
-        
-        while(k--){
-            ans = pq.top();
-            pq.pop();
-        }
-        
-        return ans;
     }
 };
 
