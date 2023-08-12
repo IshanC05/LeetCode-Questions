95ab9875f479ea81bf62c9a1eb8047ef1a005d4d
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-08-12 15:20:01 +0530

    Time: 131 ms (98.79%), Space: 73 MB (28.04%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        long long ans = 0;
-        priority_queue<int, vector<int>, greater<int>> pq1, pq2;
-        int i = 0, j = costs.size() - 1;
-
+        
+        long long ans = 0, i = 0, j = costs.size() - 1;
+        
+        priority_queue<int, vector<int>, greater<int>>p1, p2;
+        
         while(k--){
-
-            while(pq1.size() < candidates && i <= j){
-                pq1.push(costs[i++]);
+            
+            while(p1.size() < candidates && i <= j){
+                p1.push(costs[i++]);
             }
-
-            while(pq2.size() < candidates && j >= i){
-                pq2.push(costs[j--]);
+            
+            while(p2.size() < candidates && i <= j){
+                p2.push(costs[j--]);
             }
-
-            int a = (!pq1.empty()) ? pq1.top() : INT_MAX;
-            int b = (!pq2.empty()) ? pq2.top() : INT_MAX;
-
+            
+            int a = p1.size() > 0 ? p1.top() : INT_MAX;
+            int b = p2.size() > 0 ? p2.top() : INT_MAX;
+            
             if(a <= b){
-                ans += a;
-                pq1.pop();
+                ans += a; 
+                p1.pop();
             }else{
                 ans += b;
-                pq2.pop();
+                p2.pop();
             }
+            
         }
+        
         return ans;
+        
     }
-
 };
