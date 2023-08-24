221ce103631f536c04c5cf86aa661fe999f276bb
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-08-24 21:31:08 +0530

    Time: 336 ms (74.75%), Space: 142.3 MB (95.55%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,38 +1,36 @@
-class NumMatrix
-{
-    vector<vector < int>> matrix;
-    public:
-        NumMatrix(vector<vector < int>> &matrix)
-        {
-            for (int i = 0; i < matrix.size(); i++)
-            {
-                for (int j = 1; j < matrix[0].size(); j++)
-                {
-                    matrix[i][j] += matrix[i][j - 1];
-                }
-            }
+class NumMatrix {
+private:
+    vector<vector<int>> arr;
 
-            for (int i = 1; i < matrix.size(); i++)
-            {
-                for (int j = 0; j < matrix[0].size(); j++)
-                {
-                    matrix[i][j] += matrix[i - 1][j];
-                }
-            }
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
 
-            this->matrix = matrix;
+        arr = vector<vector<int>>(rows, vector<int>(cols, 0));
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int a = (i > 0) ? arr[i - 1][j] : 0;
+                int b = (j > 0) ? arr[i][j - 1] : 0;
+                int c = (i > 0 && j > 0) ? arr[i - 1][j - 1] : 0;
+                arr[i][j] = a + b - c + matrix[i][j];
+            }
         }
+    }
 
-    int sumRegion(int row1, int col1, int row2, int col2)
-    {
-        int total = matrix[row2][col2];
-        int extra = (col1 != 0 ? matrix[row2][col1 - 1] : 0) + (row1 != 0 ? matrix[row1 - 1][col2] : 0) - ((row1 != 0 && col1 != 0) ? matrix[row1 - 1][col1 - 1] : 0);
-        return total - extra;
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int a = (row1 > 0 && col1 > 0) ? arr[row1 - 1][col1 - 1] : 0;
+        int b = (row1 > 0) ? arr[row1 - 1][col2] : 0;
+        int c = (col1 > 0) ? arr[row2][col1 - 1] : 0;
+        int d = arr[row2][col2];
+
+        return d - b - c + a;
     }
 };
 
 /**
- *Your NumMatrix object will be instantiated and called as such:
- *NumMatrix* obj = new NumMatrix(matrix);
- *int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
  */
