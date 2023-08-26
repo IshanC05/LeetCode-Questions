2d6cd628331396a184951801340a58c034155dac
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-08-26 13:37:26 +0530

    Time: 211 ms (73.59%), Space: 76.1 MB (20.15%) - LeetHub

---
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        
+        long long ans = 0;
+        
+        priority_queue<int>pq;
+        
+        for(int i = 0; i < nums.size(); i++){
+            
+            pq.push(nums[i]);
+            
+        }
+        
+        while(k--){
+            
+            int top = pq.top();
+            
+            pq.pop();
+            
+            ans += top;
+            
+            pq.push(ceil(top / 3.0));
+            
+        }
+        
+        return ans;
+        
+    }
+};
