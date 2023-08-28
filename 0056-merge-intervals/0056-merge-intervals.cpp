f38e0fbf8776414315043b7bc79ed8207d294560
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-08-28 12:57:05 +0530

    Time: 155 ms (7.76%), Space: 48.2 MB (5.01%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
+    bool static comp(vector<int>a, vector<int>b){
+        
+        if(a[0] == b[0])    return a[1] < b[1];
+        
+        return a[0] < b[0];
+        
+    }
+    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(), intervals.end(), comp);
         
         vector<vector<int>>ans;
         
-        sort(intervals.begin(), intervals.end());
+        int start = intervals[0][0], end = intervals[0][1];
         
-        int start = -1, end = -1;
-        
-        for(int i = 0; i < intervals.size(); i++){       
+        for(int i = 1; i < intervals.size(); i++){
             
-            vector<int>curr = intervals[i];
+            int currStart = intervals[i][0], currEnd = intervals[i][1];
             
-            if(start == -1 && end == -1){
+            if(currStart <= end){
                 
-                start = curr[0], end = curr[1];
+                start = min(start, currStart);
                 
-                continue;
-            
-            }
-            
-            if(end >= curr[0] && end <= curr[1])    end = curr[1];
-            
-            else if(start <= curr[0] && curr[1] <= end) continue;
-            
-            else{
+                end = max(end, currEnd);
+                
+            }else{
                 
                 ans.push_back({start, end});
                 
-                start = curr[0], end = curr[1];
-            }
-            
+                start = currStart, end = currEnd;
+                
+            } 
         }
         
-        if(start != -1 && end != -1)    ans.push_back({start, end});
+        ans.push_back({start, end});
         
         return ans;
         
