7e9c6b822e5ee5b156a02b7a4ed09db791ed21c2
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-08-28 13:09:07 +0530

    Time: 32 ms (70.71%), Space: 18.9 MB (72.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
-    bool static comp(vector<int>a, vector<int>b){
-        
-        if(a[0] == b[0])    return a[1] < b[1];
-        
-        return a[0] < b[0];
-        
-    }
-    
+   
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         
-        sort(intervals.begin(), intervals.end(), comp);
+        sort(intervals.begin(), intervals.end());
         
         vector<vector<int>>ans;
         
