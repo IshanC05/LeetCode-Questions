fbbf365a3483bebedef4ff026f9b28bbc838fac3
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-09-07 10:51:30 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (12.22%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -10,50 +10,30 @@
  */
 class Solution {
 public:
-    ListNode* reverseLL(ListNode* head){
-        if(!head || !head->next){
-            return head;
-        }
-        
-        ListNode* smallHead = reverseLL(head->next);
-        ListNode* tail = head->next;
-        tail->next = head;
-        head->next = nullptr;
-        
-        return smallHead;
-    }
+      
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(!head || !head->next || left == right){
+        if (!head || left == right) {
             return head;
         }
 
-        ListNode* slow = head, *fast = head, *prevSlow = nullptr, *prevFast = nullptr;
-        
-        int i = 0;
-        while(i < right and fast){
-            prevFast = fast;
-            fast = fast->next;
-            ++i;
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
+
+        for (int i = 1; i < left; ++i) {
+            prev = prev->next;
         }
-        prevFast->next = nullptr;
-        
-        i = 1;
-        while(i < left and slow){
-            prevSlow = slow;
-            slow = slow->next;
-            ++i;
+
+        ListNode* current = prev->next;
+
+        for (int i = left; i < right; ++i) {
+            ListNode* temp = current->next;
+            current->next = temp->next;
+            temp->next = prev->next;
+            prev->next = temp;
         }
-        
-        ListNode* reverseHead = reverseLL(slow);
-        
-        if(prevSlow == nullptr){
-            head = reverseHead;
-        }else{
-            prevSlow->next = reverseHead;
-        }
-        
-        slow->next = fast;
-        
-        return head;
+
+        return dummy->next;
     }
+
 };
