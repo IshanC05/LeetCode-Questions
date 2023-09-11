502f4810b025fc849cf70c918c3a0975771945b3
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-09-11 11:49:44 +0530

    Time: 14 ms (35.21%), Space: 12.6 MB (80.59%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {      
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         
-        int maxSize = 0, n = groupSizes.size();
+        unordered_map<int,vector<int>>mp;
         
-        for(int i = 0; i < n; i++){
-            maxSize = max(maxSize, groupSizes[i]);
-        }
+        vector<vector<int>>ans;
         
-        vector<vector<int>>ans;        
-               
-        for(int i = 1; i <= maxSize; i++){
-            vector<int>temp;
-            for(int j = 0; j < n; j++){
-                if(groupSizes[j] == i){
-                    groupSizes[j] = 0;
-                    if(temp.size() < i) temp.push_back(j);
-                    else{
-                        ans.push_back(temp);
-                        temp.clear();
-                        temp.push_back(j);
-                    }
+        for(int i = 0; i < groupSizes.size(); i++){
+            
+            int gs = groupSizes[i];
+            
+            if(mp.find(gs) == mp.end()){
+                
+                mp[gs].push_back(i);
+                
+            }else{
+                
+                if(mp[gs].size() < gs){
+                    
+                    mp[gs].push_back(i);
+                
+                }else{
+                    
+                    ans.push_back(mp[gs]);
+                    
+                    mp[gs].clear();
+                    
+                    mp[gs].push_back(i);
+                
                 }
+                
             }
             
-            if(temp.size() == i)    ans.push_back(temp);
+            if(mp[gs].size() == gs){
+                
+                ans.push_back(mp[gs]);
+                
+                mp[gs].clear();
+                
+            }
             
         }
         
