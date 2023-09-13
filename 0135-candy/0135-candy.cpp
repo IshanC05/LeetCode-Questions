dc1edb605c8d55c93d20d24a9d19b78afee87c63
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-09-13 14:29:36 +0530

    Time: 18 ms (53.51%), Space: 17.7 MB (47.60%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -7,11 +7,15 @@ public:
         vector<int>count(n, 1);
         
         for(int i = 1; i < n; i++){
-            if(ratings[i] > ratings[i - 1])     count[i] = max(count[i], count[i - 1] + 1);
+            if(ratings[i] > ratings[i - 1]){
+                count[i] = max(count[i], count[i - 1] + 1);
+            }
         }
         
         for(int i = n - 2; i >= 0; i--){
-            if(ratings[i] > ratings[i + 1])     count[i] = max(count[i], count[i + 1] + 1);   
+            if(ratings[i] > ratings[i + 1]){
+                count[i] = max(count[i], count[i + 1] + 1);   
+            }
         }
         
         int ans = 0;
