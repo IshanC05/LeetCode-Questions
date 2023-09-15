df7b0c6d1a153bb73e91297d7364831c98aefb17
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-09-15 12:54:04 +0530

    Time: 15 ms (97.06%), Space: 16 MB (54.21%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
     int n;
-    void dfs(vector<vector<int>>& isConnected, int u, vector<bool>&visited){
+//     void dfs(vector<vector<int>>& isConnected, int u, vector<bool>&visited){
         
-        visited[u] = true;
+//         visited[u] = true;
         
-        for(int v = 0; v < n; v++){
-            if(!visited[v] && isConnected[u][v] == 1){
-                dfs(isConnected, v, visited);
+//         for(int v = 0; v < n; v++){
+//             if(!visited[v] && isConnected[u][v] == 1){
+//                 dfs(isConnected, v, visited);
+//             }
+//         }
+//     }
+    void bfs(vector<vector<int>>& isConnected, int s, vector<bool>&visited){
+        
+        queue<int>q;
+        q.push(s);
+        visited[s] = true;
+        
+        while(!q.empty()){
+            
+            int u = q.front();
+            q.pop();
+            
+            for(int v = 0; v < n; v++){
+                if(!visited[v] && isConnected[u][v] == 1){
+                    visited[v] = true;
+                    q.push(v);
+                }
             }
-        }
+            
+        }       
     }
     
     int findCircleNum(vector<vector<int>>& isConnected) {
         n = isConnected.size();
         
-        // Count Provinces through DFS
+        // Count Provinces through BFS
         int count = 0;
         vector<bool>visited(n, false);
         
         for(int i = 0; i < n; i++){
             if(!visited[i]){
-                dfs(isConnected, i, visited);
+                // dfs(isConnected, i, visited);
+                bfs(isConnected, i, visited);
                 ++count;
             }
         }
