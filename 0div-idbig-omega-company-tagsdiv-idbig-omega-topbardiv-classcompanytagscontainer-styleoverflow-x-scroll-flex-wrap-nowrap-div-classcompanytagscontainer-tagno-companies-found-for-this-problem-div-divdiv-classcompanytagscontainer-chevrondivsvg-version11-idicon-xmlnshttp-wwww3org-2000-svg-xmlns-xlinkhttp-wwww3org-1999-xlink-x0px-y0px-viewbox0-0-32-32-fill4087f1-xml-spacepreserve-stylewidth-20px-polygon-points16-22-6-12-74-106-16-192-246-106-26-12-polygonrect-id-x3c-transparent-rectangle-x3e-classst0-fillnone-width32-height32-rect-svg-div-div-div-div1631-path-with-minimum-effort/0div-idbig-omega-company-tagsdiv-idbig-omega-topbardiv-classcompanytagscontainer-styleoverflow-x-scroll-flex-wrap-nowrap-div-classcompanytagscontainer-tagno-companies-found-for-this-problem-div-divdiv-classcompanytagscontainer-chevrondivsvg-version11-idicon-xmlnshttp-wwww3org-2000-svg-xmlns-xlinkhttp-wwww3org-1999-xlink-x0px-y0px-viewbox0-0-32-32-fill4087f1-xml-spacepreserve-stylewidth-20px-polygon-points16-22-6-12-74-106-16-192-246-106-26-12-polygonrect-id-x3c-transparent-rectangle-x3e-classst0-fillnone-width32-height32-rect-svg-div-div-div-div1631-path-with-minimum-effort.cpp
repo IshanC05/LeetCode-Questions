e5de25758b2561e783a3f1bd71a178cb9215a98d
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-09-16 11:35:41 +0530

    Time: 103 ms (85.03%), Space: 41.5 MB (19.15%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort.cpp
@@ -1,48 +1,50 @@
 class Solution {
 public:
-    bool dfs(int i, int j, int diff, vector<vector<int>>& heights, vector<vector<bool>>& visited) {
-        int m = heights.size();
-        int n = heights[0].size();
-        
+    
+    bool dfs(int i, int j, int m, int n, int diff, vector<vector<int>>& v, vector<vector<int>>& h) {
         if (i == m - 1 && j == n - 1) {
             return true;
         }
-        
-        visited[i][j] = true;
-        
-        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        
-        for (const auto& dir : directions) {
-            int x = i + dir.first;
-            int y = j + dir.second;
-            
-            if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y] && abs(heights[x][y] - heights[i][j]) <= diff) {
-                if (dfs(x, y, diff, heights, visited)) {
-                    return true;
-                }
-            }
+
+        v[i][j] = 1;
+
+        bool path = false;
+
+        if (i + 1 < m && !v[i + 1][j] && abs(h[i][j] - h[i + 1][j]) <= diff) {
+            path = path || dfs(i + 1, j, m, n, diff, v, h);
         }
-        
-        return false;
+
+        if (j + 1 < n && !v[i][j + 1] && abs(h[i][j] - h[i][j + 1]) <= diff) {
+            path = path || dfs(i, j + 1, m, n, diff, v, h);
+        }
+
+        if (i - 1 >= 0 && !v[i - 1][j] && abs(h[i][j] - h[i - 1][j]) <= diff) {
+            path = path || dfs(i - 1, j, m, n, diff, v, h);
+        }
+
+        if (j - 1 >= 0 && !v[i][j - 1] && abs(h[i][j] - h[i][j - 1]) <= diff) {
+            path = path || dfs(i, j - 1, m, n, diff, v, h);
+        }
+
+        return path;
     }
-    
+
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int m = heights.size();
-        int n = heights[0].size();
-        int left = 0;
-        int right = INT_MAX;
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            vector<vector<bool>> visited(m, vector<bool>(n, false));
-            
-            if (dfs(0, 0, mid, heights, visited)) {
-                right = mid;
+        int l = 0, h = 1e6, ans = h, m = heights.size(), n = heights[0].size();
+
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            vector<vector<int>> v(m, vector<int>(n, 0));
+
+            if (dfs(0, 0, m, n, mid, v, heights)) {
+                ans = min(ans, mid);
+                h = mid - 1;
             } else {
-                left = mid + 1;
+                l = mid + 1;
             }
         }
-        
-        return left;
+
+        return ans;
     }
+
 };
