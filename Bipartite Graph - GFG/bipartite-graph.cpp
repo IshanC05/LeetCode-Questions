3db071569d13ea97c51ff0e40436a4b54219fb75
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-09-16 19:42:55 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -5,37 +5,42 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-    bool dfs(vector<int>adj[], int u, int uColor, vector<int>&color){
-        
-        color[u] = uColor;
-        
-        for(int &v : adj[u]){
-            
-            if(color[v] == uColor)  return false;
-            
-            if(color[v] == -1){
-                
-                int neighbourColor = 1 - uColor;
-                
-                if(dfs(adj, v, neighbourColor, color) == false)     return false;
+    bool isBipartite(int V, vector<int> adj[]) {
+        vector<int> color(V, -1);
+    
+        for (int i = 0; i < V; i++) {
+            if (color[i] == -1) {
+                if (!bfs(adj, i, 0, color))
+                    return false;
             }
         }
+    
+        return true;
+    }
+    
+    bool bfs(vector<int> adj[], int start, int startColor, vector<int>& color) {
+        queue<int> q;
+        q.push(start);
+        color[start] = startColor;
+    
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+    
+            for (int v : adj[u]) {
+                if (color[v] == -1) {
+                    color[v] = 1 - color[u];
+                    q.push(v);
+                } else if (color[v] == color[u]) {
+                    return false;
+                }
+            }
+        }
+    
         return true;
     }
 
-	bool isBipartite(int V, vector<int>adj[]){
-	    // Code here
-	    
-	    vector<int>color(V, -1);
-	    
-	    for(int i = 0; i < V; i++){
-	        if(color[i] == -1){
-	            if(dfs(adj, i, 1, color) == false)  return false;
-	        }
-	    }
-	    
-	    return true;
-	}
+
 
 };
 
