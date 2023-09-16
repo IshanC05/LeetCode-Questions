8f0815fc0976ce8533396d418f2289a5b38615fe
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-09-16 19:44:11 +0530

    Time: 19 ms (69.04%), Space: 13.9 MB (19.95%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div785-is-graph-bipartite/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div785-is-graph-bipartite.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div785-is-graph-bipartite/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div785-is-graph-bipartite.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     
-    bool dfs(vector<vector<int>>& adj, int u, int uColor, vector<int>&color){
-        
-        color[u] = uColor;
-        
-        for(int &v : adj[u]){
-            
-            if(color[v] == uColor)  return false;
-            
-            if(color[v] == -1){
-                
-                int neighbourColor = 1 - uColor;
-                
-                if(dfs(adj, v, neighbourColor, color) == false)     return false;
+    bool bfs(vector<vector<int>>& adj, int start, int startColor, vector<int>& color) {
+        queue<int> q;
+        q.push(start);
+        color[start] = startColor;
+    
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+    
+            for (int v : adj[u]) {
+                if (color[v] == -1) {
+                    color[v] = 1 - color[u];
+                    q.push(v);
+                } else if (color[v] == color[u]) {
+                    return false;
+                }
             }
         }
+    
         return true;
     }
 
 	bool isBipartite(vector<vector<int>>& adj) {
-	    // Code here
+
 	    int V = adj.size();
         
 	    vector<int>color(V, -1);
 	    
 	    for(int i = 0; i < V; i++){
 	        if(color[i] == -1){
-	            if(dfs(adj, i, 1, color) == false)  return false;
+	            if(bfs(adj, i, 1, color) == false)  return false;
 	        }
 	    }
 	    
