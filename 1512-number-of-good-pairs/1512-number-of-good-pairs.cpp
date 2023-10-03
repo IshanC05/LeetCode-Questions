9ec788bd960f1e687ebb717e97c1a4a2c2616320
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-10-03 11:28:52 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (16.85%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -4,20 +4,11 @@ public:
         
         unordered_map<int,int>mp;
         
-        for(int i : nums)
-            ++mp[i];
-        
         int count = 0;
         
-        for(int i : nums){
-            
-            if(mp[i] > 1)   count += mp[i] - 1;
-            
-            --mp[i];
-            
-            if(mp[i] == 0)  mp.erase(i);
-        }
-        
+        for(int i : nums)
+            count += mp[i]++;
+                
         return count;
     }
 };
