b4d686662a9672cc626b14cad306be8d27711aa6
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-10-11 14:11:27 +0530

    Added solution - LeetHub

--- Sort a stack - GFG/sort-a-stack.cpp
+++ Sort a stack - GFG/sort-a-stack.cpp
@@ -50,26 +50,30 @@ public:
 
 /* The below method sorts the stack s 
 you are required to complete the below method */
+void insertAtIdx(stack<int>&st, int x){
+
+	if(st.empty() || st.top() <= x){
+		st.push(x);
+		return;
+	}
+
+	int top = st.top();
+	st.pop();
+
+	insertAtIdx(st, x);
+
+	st.push(top);
+}
 void SortedStack :: sort()
 {
    //Your code here
-   if(s.size() == 1)    return;
-   
-   int top = s.top();
-   s.pop();
-   
-   sort();
-   
-   stack<int>t;
-   while(!s.empty() && s.top() > top){
-       t.push(s.top());
-       s.pop();
-   }
-   
-   s.push(top);
-   
-   while(!t.empty()){
-       s.push(t.top());
-       t.pop();
-   }
+   	if(s.size() == 1)
+		return;
+
+	int top = s.top();
+	s.pop();
+
+	sort();
+
+	insertAtIdx(s, top);
 }
