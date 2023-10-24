653c32aabec62871b3d8bac261fda328ad267f6a
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-10-24 14:48:14 +0530

    Time: 23 ms (29.79%), Space: 26.2 MB (75.94%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,35 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* helper(vector<int>in, vector<int>po, int inS, int inE, int poS, int poE){
-        if(inS > inE){
-            return NULL;
-        }
-        int rootData = po[poE];
-        TreeNode* root = new TreeNode(rootData);
+    TreeNode* helper(vector<int>& inorder, vector<int>& postorder, int start, int end, int&idx){
         
-        int rootIdx = -1;
-        for(int i = inS; i <= inE; i++){
-            if(in[i] == rootData){
-                rootIdx = i;
-                break;
-            }
+        if(start > end)     return NULL;
+        
+        int rootVal = postorder[idx--];
+        
+        int i = start;
+        
+        for(; i <= end; i++){
+            if(inorder[i] == rootVal)   break;
         }
         
-        int lInS = inS;
-        int lInE = rootIdx - 1;
-        int lPoS = poS;
-        int lPoE = lInE - lInS + lPoS;
-        int rInS = rootIdx + 1;
-        int rInE = inE;
-        int rPoS = lPoE + 1;
-        int rPoE = poE - 1;
+        TreeNode* root = new TreeNode(rootVal);
         
-        root->left = helper(in, po, lInS, lInE, lPoS, lPoE);
-        root->right = helper(in, po, rInS, rInE, rPoS, rPoE);
+        root->right = helper(inorder, postorder, i + 1, end, idx);
+        root->left = helper(inorder, postorder, start, i - 1, idx);
+        
         return root;
+        
     }
+    
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int n = inorder.size();
-        return helper(inorder, postorder, 0, n-1, 0, n-1);
+        int n = postorder.size();
+        int idx = n - 1;
+        
+        return helper(inorder, postorder, 0, n - 1, idx);
     }
 };
