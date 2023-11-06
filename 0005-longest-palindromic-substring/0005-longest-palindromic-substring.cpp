753a8f09e0cd49de1fe773a22a7880d6b29444e9
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-11-06 14:15:02 +0530

    Time: 151 ms (40.83%), Space: 302 MB (7.87%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,42 +1,46 @@
 class Solution {
 public:
-    int t[1001][1001];
-    bool isPalindrome(string &s, int i, int j){
-        
-        if(i >= j)      return true;
-        
-        if(t[i][j] != -1)   return t[i][j];
-        
-        if(s[i] != s[j])    return t[i][j] = false;
-        
-        return t[i][j] = isPalindrome(s, i + 1, j - 1);
-    }
-    
     string longestPalindrome(string s) {
-        
-        int maxSize = 0;
-        
-        int start = -1;
-        
-        int n = s.size();
-        
-        memset(t, -1, sizeof(t));
-        
-        for(int i = 0; i < n; i++){
-            
-            for(int j = i; j < n; j++){
-                
-                if(isPalindrome(s, i, j) && (j - i + 1 > maxSize)){
+        int n = s.size(), maxL = 0;
+        string res;
 
-                    maxSize = j - i + 1;
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-                    start = i;
-                }
-                
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+            int sz = 1;
+            if (maxL < sz) {
+                maxL = sz;
+                res = s[i];
             }
-            
         }
-        
-        return s.substr(start, maxSize);
+
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == s[i + 1]) {
+                dp[i][i + 1] = 1;
+                int sz = 2;
+                if (maxL < sz) {
+                    maxL = sz;
+                    res = s.substr(i, 2);
+                }
+            }
+        }
+
+        for (int length = 3; length <= n; length++) {
+            for (int i = 0; i < n - length + 1; i++) {
+                int j = i + length - 1;
+                if (s[i] == s[j] && dp[i + 1][j - 1] == 1) {
+                    dp[i][j] = 1;
+                    int currL = j - i + 1;
+                    if (maxL < currL) {
+                        maxL = currL;
+                        res = s.substr(i, currL);
+                    }
+                }
+            }
+        }
+
+        return res;
     }
+
 };
