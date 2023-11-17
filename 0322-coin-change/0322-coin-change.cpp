26d77ae8f183898f64a8fa100737a2df45b84791
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-11-17 14:30:16 +0530

    Time: 130 ms (12.00%), Space: 49.9 MB (12.24%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    int t[13][10001];
-    int helper(vector<int>& coins, int n, int sum){
-        
-	    if(sum == 0)    return 0;
-	    
-	    if(n == 0)  return INT_MAX - 1;
-        
-        if(t[n][sum] != -1)     return t[n][sum];
-	    
-	    if(coins[n - 1] <= sum){
-            
-            int take = 1 + helper(coins, n, sum - coins[n - 1]);
-            
-            int skip = helper(coins, n - 1, sum);
-            
-            return t[n][sum] = min(take, skip);            
-        }
-	        
-	    return t[n][sum] = helper(coins, n - 1, sum);
-	    
-	}
-    
     int coinChange(vector<int>& coins, int amount) {
         
         int n = coins.size();
         
-        memset(t, -1, sizeof(t));
+        vector<vector<int>>t(n + 1, vector<int>(amount + 1, 0));
         
-        int res = helper(coins, n, amount);
+        for(int i = 0; i <= n; i++)
+            t[i][0] = 0;
+        
+        for(int j = 1; j <= amount; j++)
+            t[0][j] = INT_MAX - 1;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= amount; j++){
+                
+                if(coins[i - 1] <= j){
+                    
+                    t[i][j] = min(1 + t[i][j - coins[i - 1]], t[i - 1][j]);
+                    
+                }else{
+                    
+                    t[i][j] = t[i - 1][j];
+                }
+                
+            }
+        }
+        
+        int res = t[n][amount];
         
         return res == INT_MAX - 1 ? -1 : res;
-        
     }
 };
