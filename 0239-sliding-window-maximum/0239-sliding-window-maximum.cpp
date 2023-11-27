5478306d2b754f5de5cbbd476a647331108eefab
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-11-27 19:48:17 +0530

    Time: 203 ms (70.11%), Space: 135.1 MB (48.44%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,50 +1,41 @@
-class Solution
-{
-    public:
-        vector<int> nextGreater(vector<int> &nums)
-        {
-            int n = nums.size();
-            stack<int> s;
-            vector<int> res(n);
-
-            for (int i = n - 1; i >= 0; i--)
-            {
-                while (!s.empty() and nums[s.top()] <= nums[i])
-                {
-                    s.pop();
-                }
-                if (s.empty())
-                    res[i] = n;
-                else
-                    res[i] = s.top();
-
-                s.push(i);
-            }
-            return res;
-        }
-
-    vector<int> maxSlidingWindow(vector<int> &nums, int k)
-    {
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        
+        int n = arr.size();
+        
         vector<int> ans;
-        vector<int> temp = nextGreater(nums);
-        int n = nums.size();
-
-        int j = 0;
-
-        for (int i = 0; i <= n - k; i++)
-        {
-            if (j < i)
-            {
-                j = i;
+        deque<int> dq;
+    
+        for (int i = 0; i < k; i++) {
+            // Remove elements from the back of the deque if they are smaller than the current element.
+            while (!dq.empty() && arr[i] >= arr[dq.back()]) {
+                dq.pop_back();
             }
-            while (temp[j] < i + k)
-            {
-                j = temp[j];
-            }
-
-            ans.push_back(nums[j]);
+            dq.push_back(i);
         }
-
+    
+        for (int i = k; i < n; i++) {
+            // The front element of the deque contains the maximum for the previous subarray.
+            ans.push_back(arr[dq.front()]);
+    
+            // Remove elements from the front of the deque if they are no longer in the current subarray.
+            while (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+    
+            // Remove elements from the back of the deque if they are smaller than the current element.
+            while (!dq.empty() && arr[i] >= arr[dq.back()]) {
+                dq.pop_back();
+            }
+    
+            dq.push_back(i);
+        }
+    
+        // Add the maximum of the last subarray to the 'ans' vector.
+        ans.push_back(arr[dq.front()]);
+    
         return ans;
+        
     }
 };
