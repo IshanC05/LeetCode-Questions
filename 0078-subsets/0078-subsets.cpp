d7c6c2be469117c1094a24f645ad93e5249b973a
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-11-30 21:09:45 +0530

    Time: 4 ms (37.76%), Space: 16.3 MB (26.37%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
-    void helper(vector<int>& nums, vector<int>input, int n, set<vector<int>>&res){
+    void helper(vector<int>& nums, vector<int>input, int n, vector<vector<int>>&res, int prev){
         
         if(n == 0){
-            res.insert(input);
+            res.push_back(input);
             return;
         }
         
+        while(n > 0 && nums[n - 1] == prev)     
+            --n;
+        
+        if(n == 0)  return;
+        
         input.push_back(nums[n - 1]);
         
-        helper(nums, input, n - 1, res);
+        helper(nums, input, n - 1, res, nums[n - 1]);
         
         input.pop_back();
         
-        helper(nums, input, n - 1, res);
+        helper(nums, input, n - 1, res, prev);
         
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         
-        set<vector<int>>res;
+        sort(nums.begin(), nums.end());
+        
+        vector<vector<int>>res;
         
         vector<int>input;
         
         int n = nums.size();
         
-        helper(nums, input, n, res);
+        helper(nums, input, n, res, -100);
         
-        vector<vector<int>>ans(res.begin(), res.end());
-        
-        return ans;
+        return res;
     }
 };
