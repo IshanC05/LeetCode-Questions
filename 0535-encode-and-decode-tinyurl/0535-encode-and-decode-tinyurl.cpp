b4441f9e4c4bbcf421bd8b665e5bc5ddfac80754
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-12-01 14:18:31 +0530

    Time: 3 ms (69.30%), Space: 7.1 MB (60.67%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int count = 0;
-    unordered_map<int, string>mp;
+    string s;
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        mp[++count] = longUrl;
-        return to_string(count);
+        s = longUrl + "#";
+        return s;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return mp[stoi(shortUrl)];
+        shortUrl.resize(shortUrl.size() - 1);
+        return shortUrl;
     }
 };
 
