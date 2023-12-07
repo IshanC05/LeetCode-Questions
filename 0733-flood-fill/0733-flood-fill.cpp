ac0786a2101b93387f4fdcba760d71502e3ad422
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-12-07 16:02:34 +0530

    Time: 8 ms (54.06%), Space: 14.3 MB (82.53%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    
-    void dfs(vector<vector<int>>& arr, int m, int n, int i, int j, int c1, int c2) {
-        if (i >= m || i < 0 || j >= n || j < 0 || arr[i][j] != c1 || arr[i][j] == c2)
-            return;
+    int rows[4] = {0, -1, 0, +1};
+    int cols[4] = {-1, 0, +1, 0};
 
-        arr[i][j] = c2;
+    bool isValid(int x, int y, int m, int n) {
+        return x >= 0 && y >= 0 && x < m && y < n;
+    }
 
-        dfs(arr, m, n, i + 1, j, c1, c2);
-        dfs(arr, m, n, i - 1, j, c1, c2);
-        dfs(arr, m, n, i, j + 1, c1, c2);
-        dfs(arr, m, n, i, j - 1, c1, c2);
+    void dfs(vector<vector<int>>& image, int startRow, int startCol, int m, int n, int startColor, int newColor) {
+        image[startRow][startCol] = newColor;
+
+        for (int i = 0; i < 4; i++) {
+            int newRow = startRow + rows[i];
+            int newCol = startCol + cols[i];
+
+            if (isValid(newRow, newCol, m, n) && image[newRow][newCol] == startColor) {
+                dfs(image, newRow, newCol, m, n, startColor, newColor);
+            }
+        }
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        
         int m = image.size(), n = image[0].size();
+        int startColor = image[sr][sc];
 
-        dfs(image, m, n, sr, sc, image[sr][sc], newColor);
+        if (startColor == newColor) return image;  
+
+        dfs(image, sr, sc, m, n, startColor, newColor);
 
         return image;
     }
-
-
 };
