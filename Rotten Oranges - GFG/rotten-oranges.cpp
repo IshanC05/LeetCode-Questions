7bbcfb576872594098eb8581dd9cf16806f502f3
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-12-11 17:18:38 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -6,90 +6,68 @@ using namespace std;
 class Solution 
 {
     public:
-    class Node {
-    public:
-        int x;
-        int y;
-        int t;
-    
-        Node(int x, int y, int t) {
-            this->x = x;
-            this->y = y;
-            this->t = t;
+    //Function to find minimum time required to rot all oranges. 
+    int orangesRotting(vector<vector<int>>& grid) {
+        // Code here
+        int n = grid.size(), m = grid[0].size();
+        
+        bool isFreshPresent = false;
+        
+        queue<pair<pair<int, int>, int>>q;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                
+                if(grid[i][j] == 2){
+                    
+                    grid[i][j] = 0;
+                    
+                    q.push({{i, j}, 0});
+                    
+                }
+                
+                if(grid[i][j] == 1)     isFreshPresent = true;
+            }
         }
-    };
-    
-    bool isValid(int i, int j, int r, int c) {
-        if (i < 0 || i == r || j < 0 || j == c) return false;
-        return true;
-    }
-    
-    int BFS(vector<vector<int>>& grid) {
-        queue<Node> q;
-        int row = grid.size();
-        int col = grid[0].size();
-        int ans = 0;
-    
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == 2) {
-                    Node n(i, j, 0);
-                    q.push(n);
+        
+        if(!isFreshPresent)     return 0;
+        
+        int res = INT_MIN;
+        
+        int dx[4] = {-1, 0, +1, 0};
+        int dy[4] = {0, -1, 0, +1};
+        
+        while(!q.empty()){
+            
+            int X = q.front().first.first;
+            int Y = q.front().first.second;
+            int time = q.front().second;
+            q.pop();
+            
+            res = max(res, time);
+            
+            for(int k = 0; k < 4; k++){
+                
+                int x = X + dx[k];
+                int y = Y + dy[k];
+                
+                if(x >= 0 && x < n && y >= 0 && y < m && grid[x][y] == 1){
+                    
+                    grid[x][y] = 0;
+                    
+                    q.push({{x, y}, 1 + time});
                 }
             }
         }
-    
-        while (!q.empty()) {
-            Node front = q.front();
-            q.pop();
-            int i = front.x;
-            int j = front.y;
-            int t = front.t;
-            ans = max(ans, t);
-    
-            if (isValid(i + 1, j, row, col) && grid[i + 1][j] == 1) {
-                grid[i + 1][j] = 2;
-                Node n1(i + 1, j, t + 1);
-                q.push(n1);
-            }
-    
-            if (isValid(i - 1, j, row, col) && grid[i - 1][j] == 1) {
-                grid[i - 1][j] = 2;
-                Node n1(i - 1, j, t + 1);
-                q.push(n1);
-            }
-    
-            if (isValid(i, j + 1, row, col) && grid[i][j + 1] == 1) {
-                grid[i][j + 1] = 2;
-                Node n1(i, j + 1, t + 1);
-                q.push(n1);
-            }
-    
-            if (isValid(i, j - 1, row, col) && grid[i][j - 1] == 1) {
-                grid[i][j - 1] = 2;
-                Node n1(i, j - 1, t + 1);
-                q.push(n1);
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1)     return -1;
             }
         }
-    
-        return ans;
+        
+        return res == INT_MIN ? 0 : res;
     }
-    
-    int orangesRotting(vector<vector<int>>& grid) {
-        int minTime = BFS(grid);
-    
-        int row = grid.size();
-        int col = grid[0].size();
-    
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == 1) return -1; // If there are still fresh oranges, return -1.
-            }
-        }
-    
-        return minTime;
-    }
-
 };
 
 //{ Driver Code Starts.
