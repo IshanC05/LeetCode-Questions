dade258df6d7145d3a6efacc2e22d644bc7ea8f3
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-12-11 16:01:49 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -6,14 +6,6 @@ using namespace std;
 class Solution {
   public:
     // Function to find the number of islands.
-    bool check(int x, int y, int n, int m){
-        
-        if(x < 0 || y < 0 || x >= n || y >= m)  return false;
-        
-        return true;
-        
-    }
-    
     int dx[8] = {-1, -1, 0, +1, +1, +1, 0, -1};
     int dy[8] = {0, -1, -1, -1, 0, +1, +1, +1};
     
@@ -26,12 +18,44 @@ class Solution {
             int x = i + dx[k];
             int y = j + dy[k];
             
-            if(check(x, y, n, m) && grid[x][y] == '1'){
+            if((x >= 0 && y >= 0 && x < n && y < m) && grid[x][y] == '1'){
                 
                 dfs(grid, x, y, n, m);
                 
             }
         }
+    }
+    
+    void bfs(vector<vector<char>>& grid, int i, int j, int n, int m){
+        
+        queue<pair<int, int>>q;
+        q.push({i, j});
+        
+        grid[i][j] = '0';
+        
+        while(!q.empty()){
+            
+            int X = q.front().first;
+            int Y = q.front().second;
+            q.pop();
+            
+            for(int k = 0; k < 8; k++){
+                
+                int x = X + dx[k];
+                int y = Y + dy[k];
+                
+                if((x >= 0 && x < n && y >= 0 && y < m) && grid[x][y] == '1'){
+                    
+                    grid[x][y] = '0';
+                    
+                    q.push({x, y});
+                    
+                }
+                
+            }
+            
+        }
+        
     }
     
     int numIslands(vector<vector<char>>& grid) {
@@ -45,7 +69,8 @@ class Solution {
                     
                     ++res;
                     
-                    dfs(grid, i, j, n, m);
+                    // dfs(grid, i, j, n, m);
+                    bfs(grid, i, j, n, m);
                     
                 }
                 
