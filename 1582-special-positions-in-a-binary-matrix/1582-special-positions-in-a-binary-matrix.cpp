a6640833a84c4afd1344369e7accf42f0975eb06
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-12-13 13:02:36 +0530

    Time: 15 ms (71.84%), Space: 13.4 MB (25.79%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,33 +1,43 @@
 class Solution {
-public:
-    bool check(vector<vector<int>>& mat, int i, int j, int m, int n){
-        
-        int sum = 0;
-        
-        for(int k = 0; k < n; k++)
-            sum += mat[i][k];
-        
-        if(sum > 1)     return false;
-        
-        sum = 0;
-        
-        for(int k = 0; k < m; k++)
-            sum += mat[k][j];
-        
-        return !(sum > 1); 
-    }
-    
+public:    
     int numSpecial(vector<vector<int>>& mat) {
         
-        int m = mat.size(), n = mat[0].size();
+        int m = mat.size(), n = mat[0].size(), res = 0;
         
-        int res = 0;
+        vector<int>rowCount(m), colCount(n);
+        
+        for(int i = 0; i < m; i++){
+            
+            int count = 0;
+            
+            for(int j = 0; j < n; j++){
+                
+                if(mat[i][j] == 1)  ++count;
+                
+            }
+            
+            rowCount[i] = count;
+            
+        }
+        
+        for(int j = 0; j < n; j++){
+            
+            int count = 0;
+            
+            for(int i = 0; i < m; i++){
+                
+                if(mat[i][j] == 1)  ++count;
+                
+            }
+            
+            colCount[j] = count;
+        }
         
         for(int i = 0; i < m; i++){
             
             for(int j = 0; j < n; j++){
                 
-                if(mat[i][j] == 1 && check(mat, i, j, m, n))  ++res;
+                if(mat[i][j] == 1 && rowCount[i] == 1 && colCount[j] == 1)  ++res;
                 
             }
             
