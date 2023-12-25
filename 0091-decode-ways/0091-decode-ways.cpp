54ea154378bf10390fc3283d2be95cce5ca9f98c
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-12-25 11:01:05 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (65.44%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-    int t[101];
-    int helper(string &s, int i, int n){
-        
-        if(i > n || s[i] == '0')   return 0;
-        
-        else if(i == n - 1 || i == n)     return 1;
-        
-        if(t[i] != -1)  return t[i];
-        
-        int res = helper(s, i + 1, n);
-        
-        if(i + 1 < n){
-            
-            int num = ((s[i] - '0') * 10) + (s[i + 1] - '0');
-            
-            if(num >= 1 && num <= 26)  res += helper(s, i + 2, n);
-            
-        }
-        
-        return t[i] = res;
-        
-    }
-    
     int numDecodings(string s) {
-        
         int n = s.size();
         
-        memset(t, -1, sizeof(t));
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
         
-        return helper(s, 0, n);
+        for (int i = n - 1; i >= 0; --i) {
+            if (s[i] == '0') {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i + 1];
+                if (i + 1 < n) {
+                    int num = ((s[i] - '0') * 10) + (s[i + 1] - '0');
+                    if (num >= 1 && num <= 26) {
+                        dp[i] += dp[i + 2];
+                    }
+                }
+            }
+        }
         
+        return dp[0];
     }
 };
