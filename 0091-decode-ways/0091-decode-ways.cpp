4d2d0cb7cd650f13c9f87aaa0070853439b73a7e
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-12-25 10:47:49 +0530

    Time: 3 ms (47.64%), Space: 6.4 MB (98.44%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    bool isValid(string &s, int i) {
-        if (i + 1 < s.size()) {
-            int a = s[i] - '0';
-            int b = s[i + 1] - '0';
-            return (a * 10 + b) <= 26;
-        }
-        return false;
-    }
-
     int t[101];
-    int helper(string s, int i, int n) {
-        if (i == n)  return 1;  // Reach the end of the string
-        if (s[i] == '0')  return 0;  // '0' cannot be decoded alone
-
+    int helper(string &s, int i, int n){
+        
+        if(i > n || s[i] == '0')   return 0;
+        
+        else if(i == n - 1 || i == n)     return 1;
+        
         if(t[i] != -1)  return t[i];
         
-        int single = helper(s, i + 1, n);
-        int combine = 0;
-
-        if (i + 1 < n && isValid(s, i)) {
-            combine = helper(s, i + 2, n);
+        int res = helper(s, i + 1, n);
+        
+        if(i + 1 < n){
+            
+            int num = ((s[i] - '0') * 10) + (s[i + 1] - '0');
+            
+            if(num >= 1 && num <= 26)  res += helper(s, i + 2, n);
+            
         }
-
-        return t[i] = single + combine;
+        
+        return t[i] = res;
+        
     }
-
+    
     int numDecodings(string s) {
+        
         int n = s.size();
-        if (n == 0) return 0;
+        
         memset(t, -1, sizeof(t));
+        
         return helper(s, 0, n);
+        
     }
 };
