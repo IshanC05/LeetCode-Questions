ccb32c02007743c02c4c073beab59e4b8644d439
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-12-25 11:03:47 +0530

    Time: 3 ms (47.64%), Space: 6.4 MB (87.76%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        int n = s.size();
+    int t[101];
+    int helper(string &s, int i, int n){
         
-        vector<int> dp(n + 1, 0);
-        dp[n] = 1;
+        if(s[i] == '0')   return 0;
         
-        for (int i = n - 1; i >= 0; --i) {
-            if (s[i] == '0') {
-                dp[i] = 0;
-            } else {
-                dp[i] = dp[i + 1];
-                if (i + 1 < n) {
-                    int num = ((s[i] - '0') * 10) + (s[i + 1] - '0');
-                    if (num <= 26) {
-                        dp[i] += dp[i + 2];
-                    }
-                }
+        else if(i == n - 1 || i == n)     return 1;
+        
+        if(t[i] != -1)  return t[i];
+        
+        int res = helper(s, i + 1, n);
+        
+        if(i + 1 < n){
+            
+            int num = ((s[i] - '0') * 10) + (s[i + 1] - '0');
+            
+            if(num <= 26){
+                
+                if(t[i + 2] != -1)  res += t[i + 2];
+                
+                else    res += helper(s, i + 2, n);
+                
             }
+            
         }
         
-        return dp[0];
+        return t[i] = res;
+        
+    }
+    
+    int numDecodings(string s) {
+        
+        int n = s.size();
+        
+        memset(t, -1, sizeof(t));
+        
+        return helper(s, 0, n);
+        
     }
 };
