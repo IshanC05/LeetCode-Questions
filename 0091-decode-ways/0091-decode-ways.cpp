4a896615e4a95af27da7c2d5661295abe693c48c
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-12-25 11:27:54 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (65.44%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     int t[101];
-    int helper(string &s, int i, int n){
-        
-        if(s[i] == '0')   return 0;
-        
-        else if(i == n - 1 || i == n)     return 1;
-        
-        if(t[i] != -1)  return t[i];
-        
+
+    int helper(string& s, int i, int n) {
+        if (s[i] == '0') return 0;
+        else if (i == n - 1 || i == n) return 1;
+
+        if (t[i] != -1) return t[i];
+
         int res = helper(s, i + 1, n);
-        
-        if(i + 1 < n){
-            
+
+        if (i + 1 < n) {
             int num = ((s[i] - '0') * 10) + (s[i + 1] - '0');
-            
-            if(num <= 26)  res += helper(s, i + 2, n);
-            
+            if (num <= 26) res += helper(s, i + 2, n);
         }
-        
+
         return t[i] = res;
-        
     }
-    
+
     int numDecodings(string s) {
-        
         int n = s.size();
-        
         memset(t, -1, sizeof(t));
-        
         return helper(s, 0, n);
-        
     }
 };
