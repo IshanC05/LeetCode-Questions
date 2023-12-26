ec0d5e0d76f5f5eb59f854f7d7404863a1f907a3
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-12-26 13:55:32 +0530

    Time: 69 ms (13.64%), Space: 9 MB (29.50%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -4,14 +4,12 @@ public:
         int M = 1e9 + 7;
         vector<vector<int>> t(n + 1, vector<int>(target + 1, 0));
 
-        for (int j = 1; j <= min(k, target); j++) {
-            t[1][j] = 1;
-        }
+        t[0][0] = 1;
 
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= target; j++) {
-                for (int l = 1; l <= k && l < j; l++) {
-                    t[i][j] = (t[i][j] + t[i - 1][j - l]) % M;
+                for (int l = 1; l <= k; l++) {
+                    if(j - l >= 0)  t[i][j] = (t[i][j] + t[i - 1][j - l]) % M;
                 }
             }
         }
