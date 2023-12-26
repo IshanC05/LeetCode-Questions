4c8f5ba29d49ce01e42ab5c56a4fe0cba2902659
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-12-26 12:55:54 +0530

    Time: 77 ms (10.99%), Space: 8.8 MB (65.29%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,27 +1,21 @@
 class Solution {
-public:
-    int M = 1e9+7;
-    
-    int t[31][1001];
-    
-    int helper(int n, int target, int k){
-        if(target < 1)  return 0;
-        
-        if(n == 1)  return (target <= k) ? 1 : 0;
-        
-        if(t[n][target] != -1)  return t[n][target];
-        
-        int res = 0;
-        
-        for(int i = 1; i <= k; i++){
-            res = (res + helper(n - 1, target - i, k)) % M;
-        }
-        
-        return t[n][target] = res;
-    }
-    
+public:    
     int numRollsToTarget(int n, int k, int target) {
-        memset(t, -1, sizeof(t));
-        return helper(n, target, k);
+        int M = 1e9 + 7;
+        vector<vector<int>> t(n + 1, vector<int>(target + 1, 0));
+
+        for (int j = 1; j <= k && j <= target; j++) {
+            t[1][j] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                for (int l = 1; l <= k && l < j; l++) {
+                    t[i][j] = (t[i][j] + t[i - 1][j - l]) % M;
+                }
+            }
+        }
+
+        return t[n][target];
     }
 };
