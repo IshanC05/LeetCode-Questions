45b703c503c54cd350224b613129b53838d29f18
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-01-05 12:04:24 +0530

    Time: 270 ms (39.33%), Space: 10.8 MB (62.11%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    int t[2505][2505];
-    int helper(vector<int>& nums, int i, int prev_i){
-        if(i >= nums.size())    return 0;
-        
-        if(t[i][prev_i + 1] != -1)  return t[i][prev_i + 1];
-        
-        int pick = 0, notPick = 0;
-        
-        if(prev_i == -1 || nums[i] > nums[prev_i]){
-            pick = 1 + helper(nums, i + 1, i);
-        }
-        
-        notPick = helper(nums, i + 1, prev_i);
-        
-        return t[i][prev_i + 1] = max(pick, notPick);
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
-        memset(t, -1, sizeof(t));
-        return helper(nums, 0, -1);
+        int ans = 1, n = size(nums);
+        vector<int> dp(n, 1);
+        for(int i = 0; i < n; i++) 
+            for(int j = 0; j < i; j++) 
+                if(nums[i] > nums[j]) 
+				    dp[i] = max(dp[i], dp[j] + 1), ans = max(ans, dp[i]);
+        return ans;
     }
 };
