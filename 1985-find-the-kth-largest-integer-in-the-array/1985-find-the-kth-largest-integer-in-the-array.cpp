55b6b4eceabd234feb5cf266db55244616e01648
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-01-09 17:38:01 +0530

    Time: 190 ms (91.78%), Space: 55.8 MB (66.94%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -1,39 +1,13 @@
 class Solution {
 public:
     static bool cmp(string &a, string &b){
-        if(a == b)  return true;
         if(a.length() != b.length())    return a.length() < b.length();
         
-        for(int i = 0; i < a.length(); i++){
-            if(a[i] != b[i])     return a[i] < b[i];    
-        }
-        return false;
+        return a < b;
     }
+    
     string kthLargestNumber(vector<string>& nums, int k) {
-        unordered_map<string, int>mp;
-        
-        for(string s : nums)
-            ++mp[s];
-        
-        vector<string>temp;
-        
-        for(auto it : mp)
-            temp.push_back(it.first);
-            
-        sort(temp.begin(), temp.end(), cmp);
-        
-        int i = 0, j = 0;
-        
-        while(i < temp.size()){
-            while(mp[temp[i]]){
-                nums[j++] = temp[i];
-                --mp[temp[i]];
-                
-                if(mp[temp[i]] == 0)    mp.erase(temp[i]);
-            }
-            ++i;
-        }
-        
+        sort(nums.begin(), nums.end(), cmp);
         return nums[nums.size() - k];
     }
 };
