af23be577501c80927a07b575175620bda439694
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-01-09 18:43:08 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (94.70%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
+    int t[101][101];
+    int helper(int i, int j, int r, int c){
+        if(i == r - 1 && j == c - 1)     return 1;
+        if(i < 0 || i >= r || j < 0 || j >= c)  return 0;
+        
+        if(t[i][j] != -1)   return t[i][j];
+        
+        int down = helper(i + 1, j, r, c);
+        int right = helper(i, j + 1, r, c);
+        
+        return t[i][j] = down + right;
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-
-        for (int i = 1; i < m; ++i) {
-            for (int j = 1; j < n; ++j) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
-        }
-
-        return dp[m - 1][n - 1];
+        memset(t, -1, sizeof(t));
+        return helper(0, 0, m, n);
     }
 };
