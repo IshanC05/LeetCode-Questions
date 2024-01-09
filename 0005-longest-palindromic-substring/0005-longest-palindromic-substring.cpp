24be81843706f694e5b30ada12251ad727f5ea62
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-01-09 18:17:02 +0530

    Time: 291 ms (29.10%), Space: 302.9 MB (6.59%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    int t[1001][1001];
-    bool check(string &s, int i, int j){
-        if(i >= j)  return true;
-        if(s[i] != s[j])    return false;
-        
-        if(t[i][j] != -1)   return t[i][j];
-        
-        return t[i][j] = check(s, i + 1, j - 1) == true ? 1 : 0;
-    }
     string longestPalindrome(string s) {
-        int n = s.length(), maxL = 0;
-        
-        memset(t, -1, sizeof(t));
-        
+        int n = s.size();
+        int maxL = 0;
+
         string res;
-        
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                
-                if(check(s, i, j) && maxL < (j - i + 1)){
+
+        vector<vector<int>> t(n + 1, vector<int>(n + 1, 0));
+
+        for (int gap = 0; gap < n; gap++) {
+            for (int i = 0, j = gap; j < n; i++, j++) {
+
+                if (gap == 0) {
+                    t[i][j] = 1;
+                } else if (gap == 1) {
+                    t[i][j] = (s[i] == s[j]);
+                } else if (s[i] == s[j] && t[i + 1][j - 1]) {
+                    t[i][j] = 1;
+                }
+
+                if (t[i][j] && maxL < j - i + 1) {
                     maxL = j - i + 1;
                     res = s.substr(i, maxL);
                 }
-                
             }
         }
-        
+
         return res;
     }
 };
