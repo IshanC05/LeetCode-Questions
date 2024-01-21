f3399ee77c74d7ae02c49a1b2d68dc35b33f5e20
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-01-21 11:15:37 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (12.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,17 +3,16 @@ class Solution {
         int n = nums.length;
         
         if(n == 1)  return nums[0];
-        if(n == 2)  return Math.max(nums[0], nums[1]);
         
-        int t[] = new int[n];
-        
-        t[0] = nums[0];
-        t[1] = Math.max(nums[0], nums[1]);
+        int prev0 = nums[0];
+        int prev1 = Math.max(nums[0], nums[1]);
         
         for(int i = 2; i < n; i++){
-            t[i] = Math.max(t[i - 1], t[i - 2] + nums[i]);
+            int cur = Math.max(prev1, prev0 + nums[i]);
+            prev0 = prev1;
+            prev1 = cur;
         }
         
-        return t[n - 1];
+        return prev1;
     }
 }
