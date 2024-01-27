eb6432219ecb10ea9e9801f42f2d2764b779d6c4
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-01-27 14:09:59 +0530

    Time: 1297 ms (15.93%), Space: 11.6 MB (39.56%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int M = 1e9+7;
-    int t[1001][1001];
-    int helper(int n, int k){
-        if(k == 0)  return 1;
-        if(n == 0 ) return 0;   
-        
-        if(t[n][k] != -1)   return t[n][k];
-    
-        long long res = 0;
-        
-        for(int inv = 0; inv <= min(k, n - 1); inv++){
-            res = res + helper(n - 1, k - inv);
-            res = res % M;
-        }
-        
-        return t[n][k] = res;
-    }
-    
+    const int MAXN = 1001;
+
     int kInversePairs(int n, int k) {
-        memset(t, -1, sizeof(t));
-        return helper(n, k);
+        int t[MAXN][MAXN];
+        memset(t, 0, sizeof(t));
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                if (j == 0) {
+                    t[i][j] = 1;
+                } else if (i == 0) {
+                    t[i][j] = 0;
+                } else {
+                    for (int inv = 0; inv <= min(j, i - 1); inv++) {
+                        t[i][j] = (t[i][j] + t[i - 1][j - inv]) % M;
+                    }
+                }
+            }
+        }
+
+        return t[n][k];
     }
 };
