9b9e69df4c2297d7120786a28952878aefbca6ab
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-02-03 18:27:19 +0530

    Time: 7 ms (58.48%), Space: 42.1 MB (39.03%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,21 +1,16 @@
 class Solution {
-    public int[] t = new int[501];
-    public int helper(int[] arr, int k, int idx, int n) {
-        if (idx >= n) return 0;
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        int[] dp = new int[n + 1];
 
-        if (t[idx] != -1)   return t[idx];
-
-        int maxE = arr[idx], res = 0;
-
-        for (int len = 1; len <= k && idx + len <= n; len++) {
-            maxE = Math.max(maxE, arr[idx + len - 1]);
-            res = Math.max(res, (maxE * len) + helper(arr, k, idx + len, n));
+        for (int i = 1; i <= n; i++) {
+            int maxE = arr[i - 1];
+            for (int len = 1; len <= k && i - len >= 0; len++) {
+                maxE = Math.max(maxE, arr[i - len]);
+                dp[i] = Math.max(dp[i], dp[i - len] + maxE * len);
+            }
         }
 
-        return t[idx] = res;
-    }
-    public int maxSumAfterPartitioning(int[] arr, int k) {
-        Arrays.fill(t, -1);
-        return helper(arr, k, 0, arr.length);
+        return dp[n];
     }
 }
