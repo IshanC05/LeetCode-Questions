0e9c450ce11a1642a570aaf2871aa68a80fb52ec
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-02-03 18:26:12 +0530

    Time: 5 ms (95.89%), Space: 10.8 MB (23.07%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int t[501];
-    int helper(vector<int>& arr, int k, int idx, int n) {
-        if (idx >= n) return 0;
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> dp(n + 1, 0);
 
-        if (t[idx] != -1) return t[idx];
-
-        int maxE = arr[idx], res = 0;
-
-        for (int len = 1; len <= k && idx + len <= n; len++) {
-            maxE = max(maxE, arr[idx + len - 1]);
-            res = max(res, (maxE * len) + helper(arr, k, idx + len, n));
+        for (int i = 1; i <= n; i++) {
+            int maxE = arr[i - 1];
+            for (int len = 1; len <= k && i - len >= 0; len++) {
+                maxE = max(maxE, arr[i - len]);
+                dp[i] = max(dp[i], dp[i - len] + maxE * len);
+            }
         }
 
-        return t[idx] = res;
-    }
-    int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        memset(t, -1, sizeof(t));
-        return helper(arr, k, 0, arr.size());
+        return dp[n];
     }
 };
