87fa3761741e6cac81b392d34bea47748146e4f5
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-02-12 12:39:56 +0530

    Time: 6 ms (78.24%), Space: 107.9 MB (79.50%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,19 +1,14 @@
 class Solution {
-    public long[] t;
-    public long helper(int[][] arr, int idx){
-        if(idx >= arr.length)   return 0;
-        
-        if(t[idx] != -1)    return t[idx];
-        
-        long solve = arr[idx][0] + helper(arr, idx + 1 + arr[idx][1]);
-        long skip = helper(arr, idx + 1);
-        
-        return t[idx] = Math.max(solve, skip);
-    }
     public long mostPoints(int[][] questions) {
         int n = questions.length;
-        t = new long[n];
-        Arrays.fill(t, -1);
-        return helper(questions, 0);
+        long[] t = new long[n + 1]; 
+        
+        for (int i = n - 1; i >= 0; i--) {
+            long solve = questions[i][0] + (i + 1 + questions[i][1] <= n ? t[i + 1 + questions[i][1]] : 0);
+            long skip = i + 1 < n ? t[i + 1] : 0;
+            t[i] = Math.max(solve, skip);
+        }
+        
+        return t[0];
     }
 }
