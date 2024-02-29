02c4cd7e1e9c5f399b126dd8314b1ec27379f7c7
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-02-29 10:24:46 +0530

    Time: 11 ms (66.80%), Space: 57.1 MB (86.52%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.java
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -23,6 +23,7 @@ class Solution {
         while(!q.isEmpty()){
             int qSize = q.size();
             int arr[] = new int[qSize];
+            int prev = ((level % 2 == 0) ? 0 : Integer.MAX_VALUE);
             
             for(int i = 0; i < qSize; i++){
                 TreeNode front = q.poll();
@@ -30,23 +31,21 @@ class Solution {
                 if(front.left != null)  q.offer(front.left);
                 if(front.right != null)  q.offer(front.right);
                 
-                arr[i] = front.val;
-            }
-            
-            if(level % 2 == 0){
-                for(int i = 0; i < qSize; i++){
-                    if(arr[i] % 2 == 0)     return false;
-                    if(i + 1 < qSize){
-                        if(arr[i] >= arr[i + 1])    return false;
+                int curr = front.val;
+                
+                if(level % 2 == 0){
+                    if(curr % 2 == 0)   return false;
+                    if(prev != 0){
+                        if(prev >= curr)    return false;
                     }
+                }else{
+                    if(curr % 2 != 0)     return false;
+                    if(prev != Integer.MAX_VALUE){
+                        if(prev <= curr)    return false;
+                    } 
                 }
-            }else{
-                for(int i = 0; i < qSize; i++){
-                    if(arr[i] % 2 != 0)     return false;
-                    if(i + 1 < qSize){
-                        if(arr[i] <= arr[i + 1])    return false;
-                    }                    
-                }
+                
+                prev = curr;
             }
             
             ++level;            
