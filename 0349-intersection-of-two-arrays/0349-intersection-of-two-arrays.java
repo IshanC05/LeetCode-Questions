c3e39b404031b719c3d32c75b8c55a0401184da9
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-03-10 12:47:39 +0530

    Time: 2 ms (94.84%), Space: 43 MB (72.75%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        boolean[] arr = new boolean[1001];
-        boolean[] brr = new boolean[1001];
+        boolean[] isPresent = new boolean[1001];
         
-        for(int i : nums1)  arr[i] = true;
-        for(int j : nums2)  brr[j] = true;
+        for(int i : nums1)  
+            isPresent[i] = true;
         
         List<Integer> resList = new ArrayList<>();
         
-        for(int i = 0; i < 1001; i++){
-            if(arr[i] && (arr[i] == brr[i]))    resList.add(i);
+        for(int j : nums2){
+            if(isPresent[j]){
+                resList.add(j);
+                isPresent[j] = false;
+            }
         }
         
         return resList.stream().mapToInt(Integer::intValue).toArray();
