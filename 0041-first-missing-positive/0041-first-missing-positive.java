fd6ff1f3aeeb6cc1d44379e32507d3813000e6e9
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-03-26 10:09:54 +0530

    Time: 1 ms (98.00%), Space: 54.9 MB (61.84%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,13 +1,16 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Set<Integer>set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        int n = nums.length;
+        boolean[] seen = new boolean[n + 1];
         
-        int start = 1;
-        
-        while(set.contains(start)){
-            ++start;
+        for(int num : nums){
+            if(num >= 1 && num <= n)    seen[num] = true;
         }
         
-        return start;
+        for(int i = 1; i < n + 1; i++){
+            if(!seen[i])    return i;
+        }
+        
+        return n + 1;
     }
 }
