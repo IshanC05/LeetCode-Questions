4d57b19711f909747035adf04850a45429ef6fec
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-04-08 10:29:00 +0530

    Time: 5 ms (31.29%), Space: 10.9 MB (50.25%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int len = students.size();
+        queue<int> studentQueue;
+        stack<int> sandwichStack;
+
+        for (int i = 0; i < len; i++) {
+            sandwichStack.push(sandwiches[len - i - 1]);
+            studentQueue.push(students[i]);
+        }
+
+        int lastServed = 0;
+        while (!studentQueue.empty() && lastServed < studentQueue.size()) {
+            if (sandwichStack.top() == studentQueue.front()) {
+                sandwichStack.pop();
+                studentQueue.pop();
+                lastServed = 0;
+            } else {
+                studentQueue.push(studentQueue.front());
+                studentQueue.pop();
+                lastServed++;
+            }
+        }
+
+        return studentQueue.size();
+    }
+};
