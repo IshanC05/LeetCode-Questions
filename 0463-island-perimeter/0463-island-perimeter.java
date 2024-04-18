e284428a6cc6d5cd92167c653bf2e29b913f113c
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-04-18 10:48:03 +0530

    Time: 8 ms (18.66%), Space: 45.5 MB (66.71%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,29 +1,42 @@
 class Solution {
-    public int islandPerimeter(int[][] grid) {
-        int r = grid.length;
-        int c = grid[0].length;
+    int res;
+    
+    int[] dx = {-1, 0, +1, 0};
+    int[] dy = {0, -1, 0, +1};
+    
+    public boolean check(int x, int y, int r, int c){
+        return (x >= 0 && x < r && y >= 0 && y < c);
+    }
+    
+    public void dfs(int[][] grid, int i, int j, int r, int c){
+        if(!check(i, j, r, c) || grid[i][j] == 0) {
+            ++res;
+            return;
+        }
         
-        int res = 0;
+        if (grid[i][j] == -1)
+            return;
         
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < c; j++){
-                
-                if(grid[i][j] == 1){
+        grid[i][j] = -1;
+        
+        for(int k = 0; k < 4; k++){
+
+            int x = i + dx[k];
+            int y = j + dy[k];
             
-                    res += 4;
-                    
-                    if(i > 0 && grid[i - 1][j] == 1)    
-                        --res;
-                    
-                    if(i + 1 < r && grid[i + 1][j] == 1)    
-                        --res;
-                    
-                    if(j > 0 && grid[i][j - 1] == 1)
-                        --res;
-                    
-                    if(j + 1 < c && grid[i][j + 1] == 1)    
-                        --res;
-                }
+            dfs(grid, x, y, r, c);
+        }
+    }
+    
+    public int islandPerimeter(int[][] grid) {
+        res = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == 1)     
+                    dfs(grid, i, j, rows, cols);
             }
         }
         
