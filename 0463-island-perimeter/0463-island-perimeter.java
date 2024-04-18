b59848ca29209d1d2f5681d494270a53e9a6de7c
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-04-18 10:42:01 +0530

    Time: 4 ms (99.58%), Space: 45.4 MB (81.43%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,46 +1,29 @@
 class Solution {
-    int res;
-    
-    int[] dx = {-1, 0, +1, 0};
-    int[] dy = {0, -1, 0, +1};
-    
-    boolean[][] vis;
-    
-    public boolean check(int x, int y, int r, int c){
-        return (x >= 0 && x < r && y >= 0 && y < c);
-    }
-    
-    public void dfs(int[][] grid, int i, int j, int r, int c){
-        grid[i][j] = 0;
-        vis[i][j] = true;
-        
-        for(int k = 0; k < 4; k++){
-
-            int x = i + dx[k];
-            int y = j + dy[k];
-            
-            if(!check(x, y, r, c))     
-                ++res;
-            
-            else if(grid[x][y] == 1)
-                dfs(grid, x, y, r, c);
-            
-            else if(grid[x][y] == 0)
-                if(!vis[x][y])  ++res;
-        }
-    }
-    
     public int islandPerimeter(int[][] grid) {
-        res = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
+        int r = grid.length;
+        int c = grid[0].length;
         
-        vis = new boolean[rows][cols];
+        int res = 0;
         
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(grid[i][j] != 0)     
-                    dfs(grid, i, j, rows, cols);
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                
+                if(grid[i][j] == 1){
+            
+                    res += 4;
+                    
+                    if(i > 0 && grid[i - 1][j] == 1)    
+                        --res;
+                    
+                    if(i + 1 < r && grid[i + 1][j] == 1)    
+                        --res;
+                    
+                    if(j > 0 && grid[i][j - 1] == 1)
+                        --res;
+                    
+                    if(j + 1 < c && grid[i][j + 1] == 1)    
+                        --res;
+                }
             }
         }
         
