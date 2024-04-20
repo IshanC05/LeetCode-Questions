136fc5c0deb251ac7483fd19928a70a8acd4adc1
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-04-20 16:43:36 +0530

    Time: 3 ms (100.00%), Space: 60.3 MB (69.20%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -1,41 +1,27 @@
 class Solution {
-    int[][] dirs = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-    int row2, col2;
-
-    private boolean isWithinFarm(int x, int y, int N, int M) {
-        return x >= 0 && x < N && y >= 0 && y < M;
-    }
-
-    private void DFS(int[][] land, boolean[][] visited, int x, int y) {
-        visited[x][y] = true;
-        row2 = Math.max(row2, x);
-        col2 = Math.max(col2, y);
-
-        for (int[] dir : dirs) {
-            int newX = x + dir[0], newY = y + dir[1];
-            if (isWithinFarm(newX, newY, land.length, land[0].length) && !visited[newX][newY]
-                    && land[newX][newY] == 1) {
-                DFS(land, visited, newX, newY);
-            }
-        }
-    }
-
     public int[][] findFarmland(int[][] land) {
-        boolean[][] visited = new boolean[land.length][land[0].length];
-        List<int[]> ans = new ArrayList<>();
-
-        for (int row1 = 0; row1 < land.length; row1++) {
-            for (int col1 = 0; col1 < land[0].length; col1++) {
-                if (land[row1][col1] == 1 && !visited[row1][col1]) {
-                    row2 = 0;
-                    col2 = 0;
-                    DFS(land, visited, row1, col1);
-                    int[] arr = new int[] {row1, col1, row2, col2};
-                    ans.add(arr);
+        int r = land.length;
+        int c = land[0].length;
+        
+        List<int[]> list = new ArrayList<>();
+        
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                if(land[i][j] == 1){
+                    
+                    int x = i, y = j;
+                                        
+                    for(x = i; x < r && land[x][j] == 1; x++){
+                        for(y = j; y < c && land[x][y] == 1; y++){
+                            land[x][y] = 0;
+                        }
+                    }
+                    
+                    list.add(new int[]{i, j, x - 1, y - 1});
                 }
             }
         }
-
-        return ans.toArray(new int[ans.size()][]);
+        
+        return list.toArray(new int[list.size()][]);
     }
 }
