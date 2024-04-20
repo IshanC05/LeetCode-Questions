b896d8b76591014ff6feb2ea052e07db133a7119
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-04-20 11:04:45 +0530

    Time: 17 ms (38.02%), Space: 71.5 MB (18.25%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -1,40 +1,41 @@
 class Solution {
+    int[][] dirs = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    int row2, col2;
+
+    private boolean isWithinFarm(int x, int y, int N, int M) {
+        return x >= 0 && x < N && y >= 0 && y < M;
+    }
+
+    private void DFS(int[][] land, boolean[][] visited, int x, int y) {
+        visited[x][y] = true;
+        row2 = Math.max(row2, x);
+        col2 = Math.max(col2, y);
+
+        for (int[] dir : dirs) {
+            int newX = x + dir[0], newY = y + dir[1];
+            if (isWithinFarm(newX, newY, land.length, land[0].length) && !visited[newX][newY]
+                    && land[newX][newY] == 1) {
+                DFS(land, visited, newX, newY);
+            }
+        }
+    }
+
     public int[][] findFarmland(int[][] land) {
-        int r = land.length;
-        int c = land[0].length;
-        
-        List<int[]> list = new ArrayList<>();
-        
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < c; j++){
-                if(land[i][j] == 1){
-                    
-                    land[i][j] = 0;
-                    
-                    int ei = i, ej = j;
-                    
-                    while(ei + 1 < r && land[ei + 1][j] == 1){
-                        ++ei;    
-                    }
-                    
-                    while(ej + 1 < c && land[i][ej + 1] == 1){
-                        ++ej;    
-                    }                   
-                    
-                    list.add(new int[]{i, j, ei, ej});
-                    
-                    for(int ti = i; ti <= ei; ti++){
-                        for(int tj = j; tj <= ej; tj++){
-                            land[ti][tj] = 0;
-                        }
-                    }
-                    
+        boolean[][] visited = new boolean[land.length][land[0].length];
+        List<int[]> ans = new ArrayList<>();
+
+        for (int row1 = 0; row1 < land.length; row1++) {
+            for (int col1 = 0; col1 < land[0].length; col1++) {
+                if (land[row1][col1] == 1 && !visited[row1][col1]) {
+                    row2 = 0;
+                    col2 = 0;
+                    DFS(land, visited, row1, col1);
+                    int[] arr = new int[] {row1, col1, row2, col2};
+                    ans.add(arr);
                 }
             }
         }
-        
-        int[][] res = list.toArray(new int[list.size()][]);
-        
-        return res;
+
+        return ans.toArray(new int[ans.size()][]);
     }
 }
