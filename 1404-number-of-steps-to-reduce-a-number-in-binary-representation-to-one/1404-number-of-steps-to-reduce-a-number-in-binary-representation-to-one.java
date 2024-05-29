e1a6c08235f34d1e135a999498e5fc6c0c82f261
Ishan <66194998+IshanC05@users.noreply.github.com>
2024-05-29 10:22:12 +0530

    Time: 2 ms (27.46%), Space: 41.2 MB (70.44%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,52 +1,40 @@
 class Solution {
-    public void divideBy2(StringBuilder num){
-        num.setLength(num.length() - 1);
-    }
-    
-    public static void add1(StringBuilder binary) {
-        int carry = 1;
-        int i = binary.length() - 1;
 
-        while (i >= 0) {
-            char bit = binary.charAt(i);
-            if (bit == '1' && carry == 1) {
-                binary.setCharAt(i, '0');
-                carry = 1;
-            } else if (bit == '0' && carry == 1) {
-                binary.setCharAt(i, '1');
-                carry = 0;
-                break;
-            } else {
-                break;
-            }
+    private void divideByTwo(StringBuilder s) {
+        s.deleteCharAt(s.length() - 1);
+    }
+
+    private void addOne(StringBuilder s) {
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) != '0') {
+            s.setCharAt(i, '0');
             i--;
         }
 
-        if (carry == 1) {
-            binary.insert(0, '1');
+        if (i < 0) {
+            s.insert(0, '1');
+        } else {
+            s.setCharAt(i, '1');
         }
     }
-    
+
     public int numSteps(String s) {
-        StringBuilder num = new StringBuilder(s);
-        int steps = 0;
-        
-        while(true){
-            
-            boolean isEqual = num.toString().equals("1");
-            
-            if(isEqual)     
-                break;
-            
-            if(num.charAt(num.length() - 1) == '0')
-                divideBy2(num);
-            
-            else
-                add1(num);
-        
-            ++steps;
+        StringBuilder str = new StringBuilder(s);
+
+        int operations = 0;
+        while (str.length() > 1) {
+            int N = str.length();
+
+            if (str.charAt(N - 1) == '0') {
+                divideByTwo(str);
+            } else {
+                addOne(str);
+            }
+
+            operations++;
         }
-        
-        return steps;
+
+        return operations;
     }
 }
